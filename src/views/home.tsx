1b97b6c0deb853f2e84b490bffbe9279b610fafd
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-02-25 17:26:22 +0900

    ホーム画面の追加

---
+++ src/views/home.tsx
@@ -0,0 +1,20 @@
+import '../App.css'
+import React from 'react';
+
+export function Home() {
+
+
+  return (
+    <div>
+      <h2>p5.js演習ブラウザへようこそ!!</h2>
+      このブラウザではp5.jsを用いた個人作品を展示しています。 <br /> <br />
+      
+      <h3>外部リンク</h3>
+      <a href="https://gitlab.com/RyunosukeEma">gitlab</a><br />
+      
+
+    </div>
+  )
+}
+
+export default Home
