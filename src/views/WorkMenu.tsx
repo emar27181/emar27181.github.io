a28f4bf6d1d80498833f50eb1818df57ea992efd
ema_ryu-ylab-pc <ee207008@meiji.ac.jp>
2023-03-06 19:30:31 +0900

    作品をまとめるページの追加

---
+++ src/views/WorkMenu.tsx
@@ -0,0 +1,34 @@
+import '../App.css'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export function WorkMenu() {
+  return (
+    <div>
+      <ul>
+        <NavLink activeClassName="active" to="/uniqueBrush">
+          UniqueBrush
+        </NavLink><br />
+
+        <NavLink activeClassName="active" to="/lineGame">
+          LineGame
+        </NavLink><br />
+
+        <NavLink activeClassName="active" to="/clock">
+          Clock
+        </NavLink><br />
+
+        <NavLink activeClassName="active" to="/camera">
+          Camera
+        </NavLink><br />
+
+        <NavLink activeClassName="active" to="/growArt">
+          GrowArt
+        </NavLink><br />
+      </ul>
+
+    </div>
+  )
+}
+
+export default WorkMenu
