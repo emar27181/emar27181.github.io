cc0af7e96f57ed4714b0a91955339d2cc50bac0f
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-03-07 13:48:18 +0900

    見た目の細かい修正

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -7,9 +7,9 @@ export function Home() {
   return (
     <div>
       <h2>p5.js演習ブラウザへようこそ!!</h2>
-      このブラウザではp5.jsを用いた個人作品を展示しています。 <br /> 
-      学習を目的としたブラウザであるため不備があると思いますがご容赦ください。<br />
-      
+      このブラウザではp5.jsを用いた個人作品を展示しています。 <br />
+      学習を目的としたブラウザであるため不備があると思いますがご容赦ください。<br /><br />
+
       <h3>外部リンク</h3>
       <a href="https://gitlab.com/RyunosukeEma" target="_blank">gitlab</a><br />
       <a href="https://github.com/emar27181" target="_blank">github</a><br />
