fee0c35d35a5b10f2bb12346849b1145c025e9d7
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-03-07 14:04:36 +0900

    Ballクラスの実装とテスト

--- src/pages/BallsRefrect.tsx
+++ src/pages/BallsRefrect.tsx
@@ -5,15 +5,37 @@ import React from 'react';
 export function BallsReflect() {
   const sketch = (p: P5CanvasInstance) => {
 
-    
+    class Ball {
+      x: number = 0;
+      y: number = 0;
+      r: number = 10;
+
+      constructor(x: number, y: number, r: number) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+      }
+    }
+
     p.setup = () => {
-      p.createCanvas(512, 512); 
-      p.background("#000000"); 
+      p.createCanvas(512, 512);
+      p.background("#000000");
     };
 
+    let balls: Array<Ball> = [];
+
     p.draw = () => {
-      p.fill(255); // 塗り色の設定
-      p.ellipse(p.width / 2, p.height / 2, 100, 100); // 楕円の描画(中央)
+      p.background("#000000");
+
+      //balls.push(new Ball(p.width / 2, p.height / 2, 10));
+      balls[0] = new Ball(p.width / 2, p.height / 2, 10);
+      balls[1] = new Ball(p.width / 2, 0, 10);
+
+      p.ellipse(balls[0].x, balls[0].y, balls[0].r);
+      p.ellipse(balls[1].x, balls[1].y, balls[1].r);
+
+      p.fill(255);
+      p.noStroke();
     };
   }
 
