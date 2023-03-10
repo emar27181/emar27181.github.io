3dab9cfefd347527d25ca1d5374ddc838cfecb8b
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-03-10 12:38:41 +0900

    フィールドの生成と陸の判定の修正

--- src/pages/CameraMosaicBallRPG.tsx
+++ src/pages/CameraMosaicBallRPG.tsx
@@ -9,18 +9,20 @@ export function CameraMosaicBallRPG() {
     let capture: Element;
 
     p.setup = () => {
-      p.createCanvas(512, 480);
+      p.createCanvas(512, 512);
       capture = p.createCapture(p.VIDEO);
       capture.hide();
       p.noStroke();
     };
 
     let x = 0, y = 0, dx = 4, dy = 2;
+    let hp= 100;
 
     p.draw = () => {
       let img = capture.get();
       p.image(img, 0, 0);
 
+      //フィールドの生成
       for (let i = 0; i < p.width; i += 10) {
         for (let j = 0; j < p.height; j += 10) {
           let getColor = p.get(i, j);
@@ -28,26 +30,24 @@ export function CameraMosaicBallRPG() {
           if(getColor[1] > v){v = getColor[1];}
           if(getColor[2] > v){v = getColor[2];}
 
-          if(getColor[0] > 200){p.fill(255, 0, 0);}
-          else if(getColor[1] > 200){p.fill(0, 255, 0);}
-          else if(v > 128){ p.fill(255);}
-          else{ p.fill(0);}
+          if(getColor[0] > 200){p.fill(255, 255, 0);}
+          else if(getColor[1] > 200){p.fill(255, 0, 0);}
+          else if(v > 128){ p.fill(0, 255, 0);}
+          else{ p.fill(0, 0, 255);}
           p.rect(i, j, 10,  10);
         }
       }
 
+      //プレイヤーの生成
       let nextColorX = p.get(x + dx, y);
       let nextColorY = p.get(x, y + dy);
-      if (nextColorX[0] != 0) { dx = -dx; }
-      else if (nextColorY[0] != 0) { dy = -dy; }
-      //if (nextColorX[0] != 255) { dx = -dx; }
-      //else if (nextColorY[0] != 255) { dy = -dy; }
+      if (nextColorX[2] != 0) { dx = -dx; }
+      else if (nextColorY[2] != 0) { dy = -dy; }
       if (x > p.width || x < 0) {dx = -dx;}
       else if (y > p.height || y < 0) {dy = -dy;}
       x += dx;
       y += dy;
-
-      p.fill(255, 0, 0);
+      p.fill(255, 128, 0);
       p.rect(x, y, 10, 10);
 
     };
