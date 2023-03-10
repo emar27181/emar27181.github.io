51aaf882829730e409e14ff36800b65e3884cb5f
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-03-10 12:58:04 +0900

    メニューバーの作成とダメージ判定(未完成)の作成

--- src/pages/CameraMosaicBallRPG.tsx
+++ src/pages/CameraMosaicBallRPG.tsx
@@ -13,10 +13,12 @@ export function CameraMosaicBallRPG() {
       capture = p.createCapture(p.VIDEO);
       capture.hide();
       p.noStroke();
+      p.background(255);
     };
 
+    const MENU_BAR_HEIGHT = 30;
     let x = 0, y = 0, dx = 4, dy = 2;
-    let hp= 100;
+    let hp = 100;
 
     p.draw = () => {
       let img = capture.get();
@@ -27,14 +29,14 @@ export function CameraMosaicBallRPG() {
         for (let j = 0; j < p.height; j += 10) {
           let getColor = p.get(i, j);
           let v = getColor[0];
-          if(getColor[1] > v){v = getColor[1];}
-          if(getColor[2] > v){v = getColor[2];}
+          if (getColor[1] > v) { v = getColor[1]; }
+          if (getColor[2] > v) { v = getColor[2]; }
 
-          if(getColor[0] > 200){p.fill(255, 255, 0);}
-          else if(getColor[1] > 200){p.fill(255, 0, 0);}
-          else if(v > 128){ p.fill(0, 255, 0);}
-          else{ p.fill(0, 0, 255);}
-          p.rect(i, j, 10,  10);
+          if (getColor[0] > 200) { p.fill(255, 255, 0); }
+          else if (getColor[1] > 200) { p.fill(255, 0, 0); }
+          else if (v > 128) { p.fill(0, 255, 0); }
+          else { p.fill(0, 0, 255); }
+          p.rect(i, j, 10, 10);
         }
       }
 
@@ -43,12 +45,26 @@ export function CameraMosaicBallRPG() {
       let nextColorY = p.get(x, y + dy);
       if (nextColorX[2] != 0) { dx = -dx; }
       else if (nextColorY[2] != 0) { dy = -dy; }
-      if (x > p.width || x < 0) {dx = -dx;}
-      else if (y > p.height || y < 0) {dy = -dy;}
+      if (x > p.width || x < 0) { dx = -dx; }
+      else if (y > p.height - MENU_BAR_HEIGHT || y < 0) { dy = -dy; }
       x += dx;
       y += dy;
       p.fill(255, 128, 0);
       p.rect(x, y, 10, 10);
+      console.log("x: " + x + " y: " + y);
+
+      //ダメージ判定の作成
+      let CurrentColor = p.get(x, y);
+      console.log("Current Color: " + CurrentColor);
+      if(CurrentColor[0] === 0 && CurrentColor[1] === 0 && CurrentColor[2] === 255){
+        hp--;
+      }
+
+      //メニューバーの生成
+      p.fill(255);
+      p.rect(0, p.width - MENU_BAR_HEIGHT, p.width, MENU_BAR_HEIGHT);
+      p.fill(0);
+      p.text("HP: " + hp, 0, p.height - 15);
 
     };
   }
