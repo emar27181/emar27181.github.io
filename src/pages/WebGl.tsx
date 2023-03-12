94dfd333937841a744c3257ab707268d6b9103e3
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-03-12 18:44:51 +0900

    WASDによって回転を制御する機能の追加

--- src/pages/WebGl.tsx
+++ src/pages/WebGl.tsx
@@ -6,21 +6,29 @@ export function WebGl() {
   const sketch = (p: P5CanvasInstance) => {
 
     const CANVAS_SIZE = 512;
-    
+
     p.setup = () => {
       p.createCanvas(CANVAS_SIZE, CANVAS_SIZE, p.WEBGL);
-      p.background(0); 
+      p.background(0);
     };
 
-    let angle = 0;
+    let angleX = 0, angleY = 0;
+    const ROTATE_SPEED = 0.05;
 
     p.draw = () => {
-      p.background(0)
-      p.normalMaterial()
-      p.rotateX(p.noise(angle)*7)
-      p.rotateY(angle)
-      angle = angle + 0.01
-      p.box(100)
+      p.background(0);
+      p.normalMaterial();
+      p.rotateX(angleX);
+      p.rotateY(angleY);
+
+      if (p.keyIsPressed) {
+        if (p.key === "a") { angleY -= ROTATE_SPEED; }
+        else if (p.key === "w") { angleX += ROTATE_SPEED; }
+        else if (p.key === "d") { angleY += ROTATE_SPEED; }
+        else if (p.key === "s") { angleX -= ROTATE_SPEED; }
+      }
+
+      p.box(100);
     };
   }
 
