c9a4b0b4fc2962cd876b26cf5f604a505b8ba74f
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-03-13 17:05:30 +0900

    クリック箇所から任意個の波紋の生成がされるような機能の追加

--- src/pages/Ripples.tsx
+++ src/pages/Ripples.tsx
@@ -6,6 +6,17 @@ export function Ripples() {
   const sketch = (p: P5CanvasInstance) => {
 
     const CANVAS_SIZE = 512;
+
+    class  Circle{
+      x: number;
+      y: number;
+      r: number;
+      constructor(x: number, y: number, r: number) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+      }
+    }
     
     p.setup = () => {
       p.createCanvas(CANVAS_SIZE, CANVAS_SIZE);
@@ -13,12 +24,22 @@ export function Ripples() {
     };
 
     let x = 0, y = 0, r = 0;
+    let circles: Array<Circle> = [];
 
     p.draw = () => {
+
+      if(p.mouseIsPressed){
+        circles.push(new Circle(p.mouseX, p.mouseY, 0));
+      }
+
       p.background(0);
       p.stroke(255);
       p.fill(0, 0, 0, 0); 
-      p.ellipse(x, y, r++, r++); 
+
+      for(let i = 0; i < circles.length; i++){
+        p.ellipse(circles[i].x, circles[i].y, circles[i].r++, circles[i].r++);
+      }
+      console.log(circles);
     };
   }
 
