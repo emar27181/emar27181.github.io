5a4282f0d9e85f5b75f888fa2b48fa9417fda24c
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-03-14 18:43:23 +0900

    外部リンクの埋め込みページの作成のテンプレート作品によるテスト

---
+++ src/pages/OtherLinkTest.tsx
@@ -0,0 +1,14 @@
+import '../App.css'
+import React from 'react';
+
+export function OtherLinkTest() {
+  return (
+    <div>
+      <ul>
+      <a href='https://editor.p5js.org/ema_r/sketches/_TjMj9Rql' target="_blank">テンプレートページ</a>
+      </ul>
+    </div>
+  )
+}
+
+export default OtherLinkTest
