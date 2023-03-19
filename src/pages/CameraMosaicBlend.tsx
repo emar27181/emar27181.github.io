b2bd4bc4d6064d23136dbd7d553db3012d61fc4d
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-03-19 18:20:10 +0900

    カメラ上の色を混ぜて表示するページのテンプレート実装

---
+++ src/pages/CameraMosaicBlend.tsx
@@ -0,0 +1,46 @@
+import '../App.css'
+import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
+import React from 'react';
+import { Element } from 'p5';
+
+export function CameraMosaicBlend() {
+  const sketch = (p: P5CanvasInstance) => {
+
+    let capture: Element;
+
+    p.setup = () => {
+      p.createCanvas(512, 512);
+      capture = p.createCapture(p.VIDEO);
+      capture.hide();
+      p.noStroke();
+    };
+
+    let x = 100, y = 100, dx = 20, dy = 10;
+    let splitSize = 8;
+
+    p.draw = () => {
+      p.image(capture, 0, 0);
+
+      if (p.mouseIsPressed) {
+        if (splitSize === 512) { splitSize = 8; }
+        else { splitSize *= 2; }
+      }
+
+      for (let i = 0; i < p.width; i += splitSize) {
+        for (let j = 0; j < p.height; j += splitSize) {
+          let getColor = p.get(i, j);
+
+          p.fill(getColor);
+          p.rect(i, j, splitSize, splitSize);
+        }
+      }
+
+    };
+  }
+
+  return (
+    <ReactP5Wrapper sketch={sketch} />
+  )
+}
+
+export default CameraMosaicBlend
