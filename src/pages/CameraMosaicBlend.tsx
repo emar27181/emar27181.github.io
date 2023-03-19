5a17fae55bf9c12fd1c064daea9f66af65f2116e
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-03-19 19:14:46 +0900

    ブレンドモードの変更機能の追加と描画モードの切り替え、固定値の宣言

--- src/pages/CameraMosaicBlend.tsx
+++ src/pages/CameraMosaicBlend.tsx
@@ -13,12 +13,15 @@ export function CameraMosaicBlend() {
       capture = p.createCapture(p.VIDEO);
       capture.hide();
       p.noStroke();
+      //p.frameRate(1);
     };
 
-    let x = 100, y = 100, dx = 20, dy = 10;
-    let splitSize = 8;
+    let splitSize = 8, isDrawingShape = "rect", isBackGround = false;
+    const EXPANSION = 3, ALPHA = 50;
+
 
     p.draw = () => {
+      if (isBackGround) { p.background(0); }
       p.image(capture, 0, 0);
 
       if (p.mouseIsPressed) {
@@ -26,12 +29,26 @@ export function CameraMosaicBlend() {
         else { splitSize *= 2; }
       }
 
+      if (p.keyIsPressed) {
+        if (p.key === "1") { p.blendMode(p.DIFFERENCE); }
+        else if (p.key === "2") { p.blendMode(p.DARKEST); }
+        else if (p.key === "3") { p.blendMode(p.BURN); }
+        else if (p.key === "4") { p.blendMode(p.BLEND); }
+        else if (p.key === "5") { p.blendMode(p.OVERLAY); }
+        else if (p.key === "6") { p.blendMode(p.HARD_LIGHT); }
+        else if (p.key === "7") { p.blendMode(p.SOFT_LIGHT); }
+        else if (p.key === "r") { isDrawingShape = "rect"; }
+        else if (p.key === "e") { isDrawingShape = "ellipse"; }
+        else if (p.key === "b") { isBackGround = !isBackGround;}
+      }
+
       for (let i = 0; i < p.width; i += splitSize) {
         for (let j = 0; j < p.height; j += splitSize) {
           let getColor = p.get(i, j);
 
-          p.fill(getColor);
-          p.rect(i, j, splitSize, splitSize);
+          p.fill(getColor[0], getColor[1], getColor[2], ALPHA);
+          if (isDrawingShape === "rect") { p.rect(i, j, splitSize * EXPANSION, splitSize * EXPANSION); }
+          else if (isDrawingShape === "ellipse") { p.ellipse(i, j, splitSize * EXPANSION, splitSize * EXPANSION); }
         }
       }
 
