b8db47aee48883fadc31a5fb86156787bdc55be6
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-04-14 16:06:40 +0900

    ランダムな色の生成とその制御の追加

--- src/pages/ColorGenerate.tsx
+++ src/pages/ColorGenerate.tsx
@@ -6,16 +6,37 @@ export function ColorGanerate() {
   const sketch = (p: P5CanvasInstance) => {
 
     const CANVAS_SIZE = 512;
-    
+    let isColorChanged = false;
+
     p.setup = () => {
       p.createCanvas(CANVAS_SIZE, CANVAS_SIZE);
-      p.background(0); 
+      p.background(0);
     };
 
     p.draw = () => {
-      p.fill(255); 
-      p.ellipse(p.width / 2, p.height / 2, 100, 100); 
+
+      let color = (Math.random() * 0xFFFFFF | 0).toString(16);
+      let randomColor = "#" + ("000000" + color).slice(-6);
+      let colorRandom = p.color(randomColor);
+
+      if (p.keyIsPressed) {
+        oparateKeyboard(p.key);
+      }
+
+      if (isColorChanged) {
+        p.fill(colorRandom);
+      }
+
+      p.ellipse(p.width / 2, p.height / 2, 100, 100);
     };
+
+    function oparateKeyboard(key: string) {
+      switch (key) {
+        case "c": isColorChanged = !isColorChanged; break;
+        default: break;
+      }
+    }
+
   }
 
   return (
