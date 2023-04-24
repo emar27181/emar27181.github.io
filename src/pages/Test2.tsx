bb9cdcde95c04005da1ac5bf0aa766f088014abc
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-04-24 19:08:32 +0900

    compromiseの仮実装(バグあり)

--- src/pages/Test2.tsx
+++ src/pages/Test2.tsx
@@ -1,15 +1,11 @@
+import compromise from 'compromise';
+
 export function Test2() {
-
-  /*
-  const nlp = require('compromise');
-
   const sentence = 'This is a sample sentence.';
-  const firstWord = nlp(sentence).first().text();
-
+  const firstWord = compromise(sentence).first().text();
   console.log(firstWord); // "This"
-  */
 
-  console.log("This is a test typescript file");
+  //console.log("This is a test typescript file");
 }
 
 export default Test2;
