b9cc0a563780f0d2dcef9bb97d06661f78dcab4f
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-04-25 16:38:58 +0900

    単語ごとに区切る関数のテスト使用と表示形式の変更

--- src/pages/CompromiseTemplate.tsx
+++ src/pages/CompromiseTemplate.tsx
@@ -2,15 +2,22 @@ import compromise from 'compromise';
 
 export function CompromiseTemplate() {
   const paragraph = 'This is a sample sentence by tsx. This is second sentence. This is third sentence.';
-  console.log("before: " + paragraph);
+  console.log("before: \n" + paragraph);
 
   const sentences = compromise(paragraph).sentences();
+  const terms = compromise(paragraph).terms();
+
+  console.log("after: ");
+  /*
   sentences.forEach((sentence) =>{
     console.log("after: " + sentence.text());
   });
+  */
+  console.log(sentences.map(sentence => sentence.text()));
+  console.log(terms.map(term => term.text()));
   
   //return <p>{sentences}</p>;
-  
+
   return (
     <div>
       {/* JSX elements here */}
