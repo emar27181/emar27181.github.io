c18b6d4d6595319aa9e4773e9cdbae5d92520c0b
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-04-25 17:14:24 +0900

    不要なコメントアウトの削除

--- src/pages/FormTemplate.tsx
+++ src/pages/FormTemplate.tsx
@@ -3,28 +3,12 @@ import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 export function FormTemplate() {
-  /*
-  const paragraph = 'This is a sample sentence by tsx. This is second sentence. This is third sentence.';
-  console.log("before: \n" + paragraph);
-
-  const sentences = compromise(paragraph).sentences();
-  const terms = compromise(paragraph).terms();
-
-  console.log("after: ");
-
-  console.log(sentences.map(sentence => sentence.text()));
-  console.log(terms.map(term => term.text()));
-  */
-
-
   const [colorInput, setColorInput] = useState('');
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     console.log('Color input:', colorInput);
   };
-
-  //return <p>{sentences}</p>;
 
   return (
     <Form onSubmit={handleSubmit}>
