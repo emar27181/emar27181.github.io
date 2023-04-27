52224ab166d66aaf081ffbc5f3c4330bbd78a529
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-04-27 14:46:41 +0900

    デバッグモードの追加

--- src/pages/CompromiseTemplate.tsx
+++ src/pages/CompromiseTemplate.tsx
@@ -1,28 +1,33 @@
 import compromise from 'compromise';
 import { getColorInput } from './FormTemplate';
 import { FormTemplate } from './FormTemplate';
+const DEBUG = false;
 
 export function CompromiseTemplate() {
   FormTemplate();
   //const paragraph = 'This is a sample sentence by tsx. This is second sentence. This is third sentence.';
   const paragraph = getColorInput();
-  console.log("before: \n" + paragraph);
+  if (DEBUG) {
+    console.log("before: \n" + paragraph);
+  }
 
   const sentences = compromise(paragraph).sentences();
   const terms = compromise(paragraph).terms();
   const firstChar = compromise(paragraph).firstTerms().text();
 
-  console.log("after: ");
-  console.log(sentences.map(sentence => sentence.text()));
-  console.log(terms.map(term => term.text()));
-  console.log(firstChar);
+  if (DEBUG) {
+    console.log("after: ");
+    console.log(sentences.map(sentence => sentence.text()));
+    console.log(terms.map(term => term.text()));
+    console.log(firstChar);
+  }
 
   //return <p>{sentences}</p>;
 
   return (
     <div>
-      befor(Input value): {paragraph} <br/>
-      after: blank {} <br/>
+      befor(Input value): {paragraph} <br />
+      after: blank { } <br />
     </div>
   );
 }
