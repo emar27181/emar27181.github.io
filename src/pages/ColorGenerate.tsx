35d11ff2bdfb200e292ebbfca36690fea914a622
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-04-29 00:08:03 +0900

    見た目は良くないが正しいカラーコードが出るように修正

--- src/pages/ColorGenerate.tsx
+++ src/pages/ColorGenerate.tsx
@@ -63,7 +63,7 @@ export function ColorGanerate() {
       p.text(getColorObject, 0, HUE_BAR_Y + MARGIN_HEIGHT + HUE_BAR_HEIGHT);
       let text = "hsb(" + h + "," + s + "," + b + ")" + "←なんかバグってる";
       p.text(text, 0, HUE_BAR_Y + MARGIN_HEIGHT + HUE_BAR_HEIGHT + TEXT_SIZE);
-      text = '#' + p.hex(getColor).slice(1);
+      text = '#' + p.hex(getColor);
       //text = '#' + p.hex(getColorObject); 
       //text = getColor.toString("rrggbb");
       p.text(text, 0, HUE_BAR_Y + MARGIN_HEIGHT + HUE_BAR_HEIGHT + TEXT_SIZE * 2);
