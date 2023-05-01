2abea10a5f5bdb8ad6e68e7e4e94a1eae366efac
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-05-01 20:21:47 +0900

    色を分析するロジックの仮実装

--- src/pages/CompromiseTemplate.tsx
+++ src/pages/CompromiseTemplate.tsx
@@ -1,8 +1,13 @@
 import compromise from 'compromise';
+import Color from 'color';
 const DEBUG = true;
 
 export function CompromiseTemplate() {
-  const paragraph = "This is template paragraph. This is second paragraph. Is is verbs.";
+  //const paragraph = "This is template paragraph. This is second paragraph. Is is verbs. Sample color is red and blue";
+  //const paragraph = "red and blue are test color";
+  const paragraph = "red green blue ";
+  //const paragraph = "animal";
+  const colorsArray: Array<Color> = [];
 
   if (DEBUG) { console.log("before: \ninput: " + paragraph); }
 
@@ -11,12 +16,28 @@ export function CompromiseTemplate() {
   const firstChar = compromise(paragraph).firstTerms().text();
   const doc = compromise(paragraph); //doc: 解析結果のオブジェクト
   const nouns1 = doc.nouns().map(noun => noun.text());
-  const nouns2 =doc.nouns().out('array');
+  const nouns2 = doc.nouns().out('array');
   const verbs = doc.verbs().map(verb => verb.text());
   const adverbs = doc.adverbs().map(adverb => adverb.text());
   const prepositions = doc.prepositions().map(preposition => preposition.text());
   const adjectives = doc.adjectives().map(adjective => adjective.text());
+  const colorNouns = doc.match('#Color').text();
 
+  compromise(paragraph).nouns().forEach(noun => {
+    const color = Color(noun.text());
+    if (DEBUG) {
+      console.log('color: ' + color + "(in forEach)");
+    }
+    if (color.hex() !== '#000000') {
+      colorsArray.push(color);
+    }
+  })
+
+  if (DEBUG) {
+    console.log("colorsArray: " + colorsArray);
+  }
+
+  //確認用出力
   if (DEBUG) {
     console.log("after: ");
     //console.log(sentences.map(sentence => sentence.text()));
@@ -24,12 +45,14 @@ export function CompromiseTemplate() {
     //console.log(terms.text());
     //console.log(firstChar);
     //console.log("doc: " + doc);
-    console.log("nouns1: " + nouns1);
+    //console.log("nouns1: " + nouns1);
     //console.log("nouns2: " + nouns2);
-    console.log("verbs: " + verbs);
-    console.log("adverbs: " + adverbs);
-    console.log("prepositions: " + prepositions);
-    console.log("new paragraph: " + `The ${adjectives} ${nouns1} ${verbs} over the fence.`);
+    //console.log("verbs: " + verbs);
+    //console.log("adverbs: " + adverbs);
+    //console.log("prepositions: " + prepositions);
+    //console.log("new paragraph: " + `The ${adjectives} ${nouns1} ${verbs} over the fence.`);
+    console.log("past paragraph: " + doc.sentences().toPastTense().out());
+    console.log("colorNouns: " + colorNouns);
   }
 
   return (
