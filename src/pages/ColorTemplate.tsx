6406973ff2cc8a1a57a644fa99519f62d8f6903b
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-05-03 01:53:02 +0900

    画面の表示に関する変更

--- src/pages/ColorTemplate.tsx
+++ src/pages/ColorTemplate.tsx
@@ -5,12 +5,14 @@ const ColorTemplate: React.FC = () => {
   const colors = Object.entries(colorName);
 
   return (
-    <ul>
+    <ul >
       {colors.map(([name, code]) => (
-        <ol key={name} >
-          <div>{name}</div>
+        <ul key={name} >
+          <div style={{ textAlign: 'left' }}>{name}</div>
+          <div style={{ textAlign: 'left' }}>{code[0]}, {code[1]}, {code[2]}</div>
           <div style={{ backgroundColor: `rgb(${code[0]}, ${code[1]}, ${code[2]})`, width: '30px', height: '30px' }} />
-        </ol>
+          <br />
+        </ul>
       ))}
     </ul>
   );
