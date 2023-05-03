d4fa78e2950abce2ae29ec591034c70092d97be5
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-05-03 19:30:49 +0900

    HSVを返す関数のインポートとその仮実装

--- src/pages/ColorRecommendation/ColorDisplay.tsx
+++ src/pages/ColorRecommendation/ColorDisplay.tsx
@@ -2,7 +2,7 @@ import '../../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
 //import {getColorInput} from './FormTemplate';
-import { getColorCode } from './Compromise';
+import { getColorCode, getHue, getSaturation, getValue } from './Compromise';
 
 export function ColorDisplay() {
   const sketch = (p: P5CanvasInstance) => {
@@ -12,11 +12,16 @@ export function ColorDisplay() {
     p.setup = () => {
       p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
       p.background(0); 
+      p.colorMode(p.HSB, 360, 100, 100);
     };
 
     p.draw = () => {
-      let colorInput = getColorCode();
-      p.background(colorInput);
+      const colorInput = getColorCode();
+      const hueInput = getHue();
+      const saturationInput = getSaturation();
+      const valueInput = getValue();
+
+      p.background(hueInput, saturationInput, valueInput);
       p.noFill();
       p.stroke(0);
       p.strokeWeight(3);
