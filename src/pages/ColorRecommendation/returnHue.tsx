2d9712b3e6084cc0421ca94a7658ef5473761992
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-05-04 18:42:18 +0900

    色相の色の追記

--- src/pages/ColorRecommendation/returnHue.tsx
+++ src/pages/ColorRecommendation/returnHue.tsx
@@ -5,11 +5,17 @@ export function returnHue(hueInput: string) {
 
   switch (hueInput) {
     case "red": returnHue = 0; break;
+    case "orange": returnHue = 30; break;
     case "yellow": returnHue = 60; break;
+    case "yellowgreen": returnHue = 90; break;
     case "green": returnHue = 120; break;
+    case "springgreen": returnHue = 160; break;
     case "cyan": returnHue = 180; break;
+    case "azure": returnHue = 210; break;
     case "blue": returnHue = 240; break;
+    case "violet": returnHue = 270; break;
     case "magenta": returnHue = 300; break;
+    case "rose": returnHue = 330; break;
     default: return -2; break;
   }
 
