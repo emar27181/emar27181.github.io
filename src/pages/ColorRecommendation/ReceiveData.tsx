95c571bf416bca76e916feb5925cbaaa1b38f774
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-05-19 20:22:14 +0900

    データの受け取りの初期実装(バグあり)

--- src/pages/ColorRecommendation/ReceiveData.tsx
+++ src/pages/ColorRecommendation/ReceiveData.tsx
@@ -1,14 +1,33 @@
-import '../../App.css'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export function ReceiveData() {
+const ReceiveData: React.FC = () => {
+  const [data, setData] = useState<any[]>([]);
+  console.log("This is ReceiveData.tsx")
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/data');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
-      <h1>This is receiveData.tsx</h1>
+      <h2>This is ReceiveData.tsx</h2>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default ReceiveData
