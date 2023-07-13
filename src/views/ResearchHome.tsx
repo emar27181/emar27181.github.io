84b99cd6f9583da012d7755e2bb09d7cb92c5e89
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-07-13 20:32:33 +0900

    卒業研究用のページの初期実装

--- src/views/ResearchHome.tsx
+++ src/views/ResearchHome.tsx
@@ -1,26 +1,15 @@
 import '../App.css'
 import React from 'react';
 import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
-import Template from '../pages/template';
+import BounceColorful from '../pages/BounceColorful';
 
 export function ResearchHome() {
 
 
   return (
     <div>
-      <BrowserRouter>
-        <ul>
-          <NavLink activeClassName="active" to="/template">
-            テンプレート
-          </NavLink> <br />
-
-        </ul>
-
-      </BrowserRouter>
-
-      <Switch>
-        <Template />
-      </Switch>
+      <h3>以下卒業研究用ページ</h3>
+      <BounceColorful />
     </div>
   )
 }
