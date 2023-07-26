5b01833f1ab96b898658d0ecfd9a3c0d53f1805c
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-07-26 18:52:39 +0900

    卒業研究ページの見た目の微調整

--- src/views/ResearchHome.tsx
+++ src/views/ResearchHome.tsx
@@ -12,7 +12,7 @@ export function ResearchHome() {
     <div>
       <h3>以下卒業研究用ページ</h3>
       <BounceColorful />
-      <DisplayEmotionColorRatio />
+      <DisplayEmotionColorRatio /> <br />
       <ExplainPluticCircle /> <br />
       <a href="https://docs.google.com/presentation/d/1N21mXeW3NV-ODW7jHCPQ2FYsSbpZpuSNzAaHix2wRyc/edit?usp=sharing" target="_blank">卒業研究 説明スライド</a><br />
     </div>
