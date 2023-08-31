2f84b9aad34b3dc5fb9d11f513ee1b780f97da8f
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-08-31 14:06:37 +0900

    描画キャンバスの初期実装

--- src/pages/Reserch/Canvas.tsx
+++ src/pages/Reserch/Canvas.tsx
@@ -1,6 +1,12 @@
+/*このプログラムは研究用のキャンバスを提示するプログラムです
+  作成日: 2023/08/31 
+*/
+
 import '../../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
+
+const IS_NO_STROKE = true;
 
 export function Canvas() {
   const sketch = (p: P5CanvasInstance) => {
@@ -10,11 +16,14 @@ export function Canvas() {
     p.setup = () => {
       p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
       p.background(0); 
+      if(IS_NO_STROKE){p.noStroke();}
     };
 
     p.draw = () => {
       p.fill(255); 
-      p.ellipse(p.width / 2, p.height / 2, 100, 100); 
+
+      //p.ellipse(p.width / 2, p.height / 2, 100, 100); 
+      p.ellipse(p.mouseX, p.mouseY, 10, 10); 
     };
   }
 
