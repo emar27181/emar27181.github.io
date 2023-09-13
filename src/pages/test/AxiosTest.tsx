17634a07cc3caedb6dea37f13c2b8c0f3a939cbe
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-09-13 20:07:52 +0900

    テスト出力の追加

--- src/pages/test/AxiosTest.tsx
+++ src/pages/test/AxiosTest.tsx
@@ -1,25 +1,14 @@
 import '../../App.css'
-import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
+import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
+//import Template from '../pages/template';
 
 export function AxiosTest() {
-  const sketch = (p: P5CanvasInstance) => {
-
-    const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
-    
-    p.setup = () => {
-      p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-      p.background(0); 
-    };
-
-    p.draw = () => {
-      p.fill(255); 
-      p.ellipse(p.width / 2, p.height / 2, 100, 100); 
-    };
-  }
 
   return (
-    <ReactP5Wrapper sketch={sketch} />
+    <div>
+      <h2> This is AxiosTest page!!</h2>
+    </div>
   )
 }
 
