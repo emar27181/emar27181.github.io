8f66fb0d3a32c749e5daa1b293064f38cf0712bb
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-09-19 20:37:12 +0900

    色の割合の比率を表示させるファイルもバッグエンドから取得した情報を反映するように実装

--- src/pages/Reserch/DisplayEmotionColorRatio.tsx
+++ src/pages/Reserch/DisplayEmotionColorRatio.tsx
@@ -1,10 +1,14 @@
 import '../../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
+import axios from 'axios';
+
 
 // データの取得
+/*
 const response = await fetch('src/pages/ColorRecommendation/data/ColorIntenseData.json');
 const DATA = await response.json();
+*/
 //console.log("DATA: " + DATA);// 確認用出力
 
 export function DisplayEmotionColorRatio() {
@@ -15,6 +19,7 @@ export function DisplayEmotionColorRatio() {
     let intense: number[] = [];
     let colorWidth: number[] = [];
     let sumIntense = 0;
+    const DEBUG = false;
 
     p.setup = () => {
       p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -24,6 +29,7 @@ export function DisplayEmotionColorRatio() {
       p.noStroke();
 
       //hueとintenseの値を代入
+      /*
       for (let i = 0; i < 8; i++) {
 
         let data = DATA[i];
@@ -33,11 +39,16 @@ export function DisplayEmotionColorRatio() {
         //console.log("data[" + i + "]: " + data);
         //console.log("hue[" + i + "]: " + hue + ", intense[" + i + "]: " + intense);
       }
+      */
+
+      fetchData();
 
       //描画する横幅の計算と代入
+      /*
       for (let i = 0; i < 8; i++) {
         colorWidth[i] = CANVAS_WIDTH * intense[i] / sumIntense;
-        //console.log("colorWidth[" + i + "] = " + colorWidth[i]);
+        console.log('intense[i]: ' + intense[i]);
+        console.log("colorWidth[" + i + "] = " + colorWidth[i]);
       }
 
       //色の割合に基づいて描画
@@ -48,15 +59,60 @@ export function DisplayEmotionColorRatio() {
 
         startWidth += colorWidth[i];
         endWidth += colorWidth[i];
-       //console.log("hue[i] = " + hue[i] + ", startWidth: " + startWidth + ", endWidth: " + endWidth);
+        //console.log("hue[i] = " + hue[i] + ", startWidth: " + startWidth + ", endWidth: " + endWidth);
       }
+      */
+      //displayColorPalette();
 
     };
 
 
 
     p.draw = () => {
+      displayColorPalette();
+    };
 
+    function displayColorPalette() {
+
+      //描画する横幅の計算と代入
+      for (let i = 0; i < 8; i++) {
+        colorWidth[i] = CANVAS_WIDTH * intense[i] / sumIntense;
+        if (DEBUG) { console.log("colorWidth[" + i + "] = " + colorWidth[i]); }
+      }
+
+      //色の割合に基づいて描画
+      let startWidth = 0, endWidth = colorWidth[0];
+      for (let i = 0; i < 8; i++) {
+        p.colorMode(p.HSB, 360, 100, 100, 100);
+        p.fill(hue[i], 80, 100, 255);
+        p.rect(startWidth, 0, endWidth, CANVAS_HEIGHT);
+
+        startWidth += colorWidth[i];
+        endWidth += colorWidth[i];
+        if (DEBUG) { console.log("hue[i] = " + hue[i] + ", startWidth: " + startWidth + ", endWidth: " + endWidth); }
+      }
+    }
+
+    async function fetchData() {
+      try {
+        console.log("fetchData is called");
+        const response = await axios.get('http://localhost:5000/api/send-data');
+        const jsonData = response.data;
+        const parsedData = JSON.parse(jsonData.message);
+
+        //hueとintenseの値を代入
+        for (let i = 0; i < 8; i++) {
+          let data = parsedData[i];
+          hue[i] = data.hue;
+          intense[i] = data.intense;
+          sumIntense += data.intense;
+          console.log('hue[i]: ' + hue[i]);
+          console.log('intense[i]: ' + intense[i]);
+        }
+
+      } catch (error) {
+        console.error('エラーが発生しました:', error);
+      }
     };
   }
 
