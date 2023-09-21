5ace626bb48adc25d1232a23f00e0561edc37c8c
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-09-21 18:58:11 +0900

    描画方法に関する説明する文章を表示する機能を実装

--- src/pages/Reserch/ExplainDrawing.tsx
+++ src/pages/Reserch/ExplainDrawing.tsx
@@ -3,11 +3,18 @@ import React from 'react';
 import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
 
 export function ExplainDrawing() {
-
+  const paragraphStyle = {
+    lineHeight: '0px', // 任意のピクセル数を指定
+  };
 
   return (
     <div>
-      <h2> This is template page!!</h2>
+      <br />
+      <p style={paragraphStyle}>0~8: 描画する感情の色の切り替え</p>
+      <p style={paragraphStyle}>s: スポイト機能(現在未動作)</p>
+      <p style={paragraphStyle}>e: キャンバスのスクリーンショットの保存</p>
+      <p style={paragraphStyle}>m: 描画インクの移動の切り替え</p>
+      <p style={paragraphStyle}>+/-: 描画サイズの拡大縮小</p>
     </div>
   )
 }
