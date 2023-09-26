e1392ac0d9b65c6c86c7b24e7005470b8e1e3b5c
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-09-26 15:51:37 +0900

    感情の色の日本語訳が表示される仮実装

--- src/views/ResearchHome.tsx
+++ src/views/ResearchHome.tsx
@@ -26,13 +26,11 @@ export function ResearchHome() {
       <ExplainDrawing />
       <Canvas />
       <DisplayEmotionColorRatio /> <br />
-      <ExplainPluticCircleEnglish />
-      {/*
       <div style={{ display: 'flex' }}>
         <ExplainPluticCircleEnglish />
         <ExplainPluticCircle /> <br />
       </div>
-  */}
+  
       <a href="https://docs.google.com/presentation/d/1N21mXeW3NV-ODW7jHCPQ2FYsSbpZpuSNzAaHix2wRyc/edit?usp=sharing" target="_blank">卒業研究 説明スライド</a><br />
     </div>
   )
