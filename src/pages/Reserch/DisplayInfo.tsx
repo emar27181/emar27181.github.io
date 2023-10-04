b7af490d4ca53a5240b342b6a70b2e360532df76
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-10-04 17:00:46 +0900

    現在の入力文章が表示されるように修正

--- src/pages/Reserch/DisplayInfo.tsx
+++ src/pages/Reserch/DisplayInfo.tsx
@@ -1,52 +1,26 @@
 import '../../App.css'
 import React from 'react';
-import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-
-import axios from 'axios';
 const DEBUG = false;
 
 export function DisplayInfo() {
-  //console.log('This is DisplayInfo.tsx');
-
-
-  fetchData();
-  let jsonData, jsonDataString: string = "";
-  let testString: string = "This is testString";
-
-  // バックエンドからJSONデータの取得
-  async function fetchData() {
-
-    try {
-      const response = await axios.get('http://localhost:5000/api/input-sentence-now');
-
-      jsonData = response.data;
-      jsonDataString = JSON.stringify(jsonData);
-
-      console.log("jsonDataString: " + jsonDataString);
-      if (DEBUG) {
-        //console.log("response:" + JSON.stringify(response));
-        //console.log(response);
-        //console.log("jsonData: " + jsonData);
-        //console.log("typeof(jsonData): " + typeof (jsonData));
-        //console.log("jsonDataString: " + jsonDataString);
-        //console.log("typeof(jsonDataString): " + typeof (jsonDataString));
-      }
-
-    } catch (error) {
-      console.error('エラーが発生しました:', error);
-    }
-
-  }
+  const [data, setData] = useState(""); // データを保存するための状態
+  useEffect(() => {
+    // Flaskのエンドポイントからデータを取得
+    fetch("http://localhost:5000/api/input-sentence-now")
+      .then((response) => response.text())
+      .then((data) => {
+        // 取得したデータを状態に保存
+        setData(data);
+      })
+      .catch((error) => {
+        console.error("データの取得エラー:", error);
+      });
+  }, []);
 
   return (
     <div>
-      {/*<p>{"jsonData: " + JSON.stringify(jsonData)}</p>}
-      {"jsonDataString: " + { jsonDataString }}
-      <br />
-      {/*"testString: " + testString*/}
-      <a> ここに現在読み込まれている文章を表示させたいがバグってる...</a>
-
+      {"現在の入力文章:  " + data}
     </div>
   )
 }
