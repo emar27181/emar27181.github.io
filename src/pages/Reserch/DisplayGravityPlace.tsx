5b65e8f17682d652a7b35570c37f0737cd886c57
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-10-21 20:12:59 +0900

    固定された重力場が存在するかどうかを判別する変数のインポート

--- src/pages/Reserch/DisplayGravityPlace.tsx
+++ src/pages/Reserch/DisplayGravityPlace.tsx
@@ -1,11 +1,13 @@
 import '../../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
+import { ReturnIsFixedGravity } from './Canvas';
 
 export function DisplayGravityPlace() {
   const sketch = (p: P5CanvasInstance) => {
 
     const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
+    let isFixedGravity = ReturnIsFixedGravity();
 
     p.setup = () => {
       p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -13,10 +15,10 @@ export function DisplayGravityPlace() {
     };
 
     p.draw = () => {
+      isFixedGravity = ReturnIsFixedGravity();
       p.textSize(20);
       p.fill(255);
-      //p.text("this is test text. \n aaa", p.width / 2, p.height / 2);
-      p.text("this is test text. \n aaa", 0, 20);
+      p.text("isFixGravity: \n" + isFixedGravity, 0, 20);
     };
   }
 
