d09fff42280e6533fe90c14b49f3079e536ffb43
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-10-24 18:40:55 +0900

    画像データのエクスポート(ReturnCameraInfoのコピー)

--- src/pages/Reserch/ReturnImageInfo.tsx
+++ src/pages/Reserch/ReturnImageInfo.tsx
@@ -3,10 +3,14 @@ import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
 import p5 from 'p5'
 
+let colorsInfo: Array<ColorInfo> = [];
+let isGetColors = false;
+
 export function ReturnImageInfo() {
   const sketch = (p: P5CanvasInstance) => {
 
     const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
+    const DIV_VALUE = 100;
     let img: p5.Image;
 
     p.preload = () => {
@@ -20,7 +24,27 @@ export function ReturnImageInfo() {
 
     p.draw = () => {
 
+      if (isGetColors) {
+        
+      }
+      else {
+        getColors();
+        isGetColors = true;
+      }
     };
+
+    function getColors() {
+      let indexNum = 0;
+      let intervalLength = p.width / DIV_VALUE;
+      for (let i = 0; i < DIV_VALUE; i++) {
+        for (let j = 0; j < DIV_VALUE; j++) {
+          //console.log(p.get(i, j));
+          let x = p.width * (i / DIV_VALUE);
+          let y = p.height * (j / DIV_VALUE);
+          colorsInfo[indexNum++] = new ColorInfo(x, y, p.get(x, y));
+        }
+      }
+    }
   }
 
   return (
@@ -39,5 +63,7 @@ export class ColorInfo {
     this.color = color;
   }
 }
+
+export function ReturnImageColorsInfo() { return colorsInfo; }
 
 export default ReturnImageInfo
