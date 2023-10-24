b5fa6c29088055c45c9a1aae4bbafd84f5e166ae
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-10-24 18:25:46 +0900

    色の情報を保存するクラスの再宣言

--- src/pages/Reserch/ReturnImageInfo.tsx
+++ src/pages/Reserch/ReturnImageInfo.tsx
@@ -28,4 +28,16 @@ export function ReturnImageInfo() {
   )
 }
 
+export class ColorInfo {
+  x: number;
+  y: number;
+  color: number[];
+
+  constructor(x: number, y: number, color: number[]) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
+}
+
 export default ReturnImageInfo
