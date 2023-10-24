ab6c73993d64fd6e5991a6ce9acf29712a710193
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-10-24 18:13:56 +0900

    画像を読み込んで表示させる機能の初期実装

--- src/pages/TestDisplayImage.tsx
+++ src/pages/TestDisplayImage.tsx
@@ -1,20 +1,25 @@
 import '../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
+import p5 from 'p5'
 
 export function TestDisplayImage() {
   const sketch = (p: P5CanvasInstance) => {
 
     const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
-    
+    let img: p5.Image;
+
+    p.preload = () => {
+      img = p.loadImage("src/data/saveCanvas-2023-10-23T130343.793.png")
+    }
+
     p.setup = () => {
       p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-      p.background(0); 
+      p.image(img, 0, 0);
     };
 
     p.draw = () => {
-      p.fill(255); 
-      p.ellipse(p.width / 2, p.height / 2, 100, 100); 
+
     };
   }
 
