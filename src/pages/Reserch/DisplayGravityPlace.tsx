4d61b82e3c8e0dfc97e8c24a753247f118563755
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-10-29 02:08:32 +0900

    それぞれの手の親指と人差し指の座標の距離に対して色が変化するように変更

--- src/pages/Reserch/DisplayGravityPlace.tsx
+++ src/pages/Reserch/DisplayGravityPlace.tsx
@@ -53,10 +53,12 @@ export function DisplayGravityPlace() {
       setColor(isMouseGravity);
       DrawGravityPlace(mouseX, mouseY, canvasWidth, canvasHeight);
 
-      if (isTrackingGravity()) { p.fill(255, 0, 0); }
+      if (isTrackingGravity(trackingX1, trackingY1, trackingX2, trackingY2)) { p.fill(255, 0, 0); }
       else { p.fill(0, 255, 0); }
       DrawGravityPlace(trackingX1, trackingY1, 300, 150);
       DrawGravityPlace(trackingX2, trackingY2, 300, 150);
+      if (isTrackingGravity(trackingX3, trackingY3, trackingX4, trackingY4)) { p.fill(255, 0, 0); }
+      else { p.fill(0, 255, 0); }
       DrawGravityPlace(trackingX3, trackingY3, 300, 150);
       DrawGravityPlace(trackingX4, trackingY4, 300, 150);
     };
