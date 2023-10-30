b4b664f160664811e9abced7014d6dce0734dc6d
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-10-30 11:07:53 +0900

    薬指と小指のトラッキングの座標を返す機能の実装

--- src/pages/TestDrawCanvas.tsx
+++ src/pages/TestDrawCanvas.tsx
@@ -42,7 +42,7 @@ export function GetTrackingData(ctx: CanvasRenderingContext2D, handLandmarks: No
   const CANVAS_HEIGHT = CANVAS_SIZE[1];
   const RATIO_X = CANVAS_WIDTH / width // 描画キャンバスとの比率
   const RATIO_Y = CANVAS_HEIGHT / height // 描画キャンバスとの比率
-  let x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0
+  let x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0, x4 = 0, y4 = 0, x5 = 0, y5 = 0
   let firstHand = [0, 0, 0, 0], secondHand = [0, 0, 0, 0];
 
   if (handLandmarks.length >= 1 && handLandmarks[0].length > 8) {
@@ -55,12 +55,20 @@ export function GetTrackingData(ctx: CanvasRenderingContext2D, handLandmarks: No
     x2 = handLandmarks[0][4].x * width
     x2 = (width - x2) * RATIO_X// x座標を反転(左右反転)
     y2 = handLandmarks[0][4].y * height * RATIO_Y
-    firstHand = [x1, y1, x2, y2];
+    //firstHand = [x1, y1, x2, y2];
     //中指の座標の取得
     x3 = handLandmarks[0][12].x * width
     x3 = (width - x3) * RATIO_X// x座標を反転(左右反転)
     y3 = handLandmarks[0][12].y * height * RATIO_Y
-    firstHand = [x1, y1, x2, y2, x3, y3];
+    //薬指の座標の取得
+    x4 = handLandmarks[0][16].x * width
+    x4 = (width - x4) * RATIO_X// x座標を反転(左右反転)
+    y4 = handLandmarks[0][16].y * height * RATIO_Y
+    //小指の座標の取得
+    x5 = handLandmarks[0][20].x * width
+    x5 = (width - x5) * RATIO_X// x座標を反転(左右反転)
+    y5 = handLandmarks[0][20].y * height * RATIO_Y
+    firstHand = [x1, y1, x2, y2, x3, y3, x4, y4, x5, y5];
 
     //二つ目の手の認識
     if (handLandmarks.length === 2 && handLandmarks[0].length > 8 && handLandmarks[1].length > 8) {
@@ -76,7 +84,15 @@ export function GetTrackingData(ctx: CanvasRenderingContext2D, handLandmarks: No
       x3 = handLandmarks[1][12].x * width
       x3 = (width - x3) * RATIO_X// x座標を反転(左右反転)
       y3 = handLandmarks[1][12].y * height * RATIO_Y
-      secondHand = [x1, y1, x2, y2, x3, y3]
+      //薬指の座標の取得
+      x4 = handLandmarks[0][16].x * width
+      x4 = (width - x4) * RATIO_X// x座標を反転(左右反転)
+      y4 = handLandmarks[0][16].y * height * RATIO_Y
+      //小指の座標の取得
+      x5 = handLandmarks[0][20].x * width
+      x5 = (width - x5) * RATIO_X// x座標を反転(左右反転)
+      y5 = handLandmarks[0][20].y * height * RATIO_Y
+      secondHand = [x1, y1, x2, y2, x3, y3, x4, y4, x5, y5]
     }
   }
   //return [[x1, y1, x2, y2], [x3, y3, x4, y4]]
