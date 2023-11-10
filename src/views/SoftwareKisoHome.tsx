76a787c823d919980a739550f567baba08e42886
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-10 15:30:10 +0900

    マッチ棒ゲームのテンプレート実装

--- src/views/SoftwareKisoHome.tsx
+++ src/views/SoftwareKisoHome.tsx
@@ -2,28 +2,14 @@ import '../App.css'
 import React from 'react';
 import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
 import Template from '../pages/template';
+import MatchBoGame from '../pages/MatchBoGame';
 
 export function SoftwareKisoHome() {
 
 
   return (
     <div>
-      <h2> This is template page!!</h2>
-      <BrowserRouter>
-        <ul>
-          <NavLink activeClassName="active" to="/template">
-            テンプレートページ
-          </NavLink> <br />
-
-        </ul>
-
-        <Switch>
-          <Route path="/template" >
-            <Template />
-          </Route>
-        </Switch>
-
-      </BrowserRouter>
+      <MatchBoGame />
     </div>
   )
 }
