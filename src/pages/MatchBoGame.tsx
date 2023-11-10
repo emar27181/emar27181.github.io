18a94d9f18906051ada71281b835769b057ff788
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-10 16:02:53 +0900

    マッチ棒の攻撃を行う関数の初期実装

--- src/pages/MatchBoGame.tsx
+++ src/pages/MatchBoGame.tsx
@@ -8,7 +8,6 @@ export function MatchBoGame() {
     const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
 
     let matchValue = [[1, 1], [1, 1]];
-    console.log(matchValue);
 
     p.setup = () => {
       p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -16,11 +15,39 @@ export function MatchBoGame() {
     };
 
     p.draw = () => {
+      p.frameRate(0.3);
       p.background(0);
       p.fill(255);
       p.textSize(30);
-      p.text(matchValue[0][0] + ", " + matchValue[0][1] + "\n\n" + matchValue[1][0] + ", " + matchValue[1][1], p.width / 2 - 15, p.height / 2 - 15);
+      p.text(matchValue[0][0] + ", " + matchValue[0][1] + "\n\n" + matchValue[1][0] + ", " + matchValue[1][1], 0, p.height / 2 - 15);
+
+      if (p.frameCount % 2 === 0) { attackMatchBo(0, 1); }
+      else { attackMatchBo(1, 0); }
     };
+
+    function attackMatchBo(attackSideNumber: number, receiveSideNumber: number) {
+      let attackHandNumber: number; // どちらの手で攻撃するかを保存する変数
+
+      // 自分のどちらの手で攻撃するかの決定
+      // 0で攻撃する場合
+      if (p.random() < 0.5) {
+        attackHandNumber = 0;
+      }
+      // 1で攻撃する場合
+      else {
+        attackHandNumber = 1;
+      }
+
+      // 相手のどちらかの手を攻撃するかの決定と攻撃
+      // 0を攻撃する場合
+      if (p.random() < 0.5) {
+        matchValue[receiveSideNumber][0] += matchValue[attackSideNumber][attackHandNumber];
+      }
+      // 1を攻撃する場合
+      else {
+        matchValue[receiveSideNumber][1] += matchValue[attackSideNumber][attackHandNumber];
+      }
+    }
   }
 
   return (
