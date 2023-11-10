2706bf048f83681392bf671296673e886aedbb06
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-10 15:38:53 +0900

    マッチ棒の値を表示する機能の初期実装

--- src/pages/MatchBoGame.tsx
+++ src/pages/MatchBoGame.tsx
@@ -7,14 +7,19 @@ export function MatchBoGame() {
 
     const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
 
+    let matchValue = [[1, 1], [1, 1]];
+    console.log(matchValue);
+
     p.setup = () => {
       p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
       p.background(0);
     };
 
     p.draw = () => {
+      p.background(0);
       p.fill(255);
-      p.ellipse(p.width / 2, p.height / 2, 100, 100);
+      p.textSize(30);
+      p.text(matchValue[0][0] + ", " + matchValue[0][1] + "\n\n" + matchValue[1][0] + ", " + matchValue[1][1], p.width / 2 - 15, p.height / 2 - 15);
     };
   }
 
