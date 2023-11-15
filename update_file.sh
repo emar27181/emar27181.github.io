2fd3e8f27bd5b6bbebbf37f7ef8488d71f54e11f
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-15 17:37:01 +0900

    404ページが更新されるように修正

--- update_file.sh
+++ update_file.sh
@@ -1,6 +1,8 @@
 rm docs/index.html
+rm docs/404.html
 rm docs/assets/*.js
 rm docs/assets/*.css
 cp dist/index.html docs/
+cp dist/index.html docs/404.html
 cp dist/assets/*.js docs/assets/
 cp dist/assets/*.css docs/assets/
