3363c9b7cbc14f7a575de87787825b3a2acf43b5
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-15 15:51:23 +0900

    ページ幅の比率によって表示を変える機能の実装

--- src/views/TestResponsiveHome.tsx
+++ src/views/TestResponsiveHome.tsx
@@ -7,17 +7,22 @@ import { useMediaQuery } from 'react-responsive';
 import TestPageForDesktopComponent from '../pages/TestPageForDesktopComponet';
 import TestPageForMobileComponent from '../pages/TestPageForMobileComponent';
 
+
+let isDesktop = false;
 export function TestResponsiveHome() {
+
 
   useEffect(() => {
     // ウィンドウのサイズが変更されたときに実行されるハンドラーを登録
     const handleResize = () => {
       const windowWidth = window.innerWidth;
       const windowHeight = window.innerHeight;
+      isDesktop = windowWidth > windowHeight;
 
       // ここで取得した windowWidth と windowHeight を使って必要な処理を行う
       console.log('Window Width:', windowWidth);
       console.log('Window Height:', windowHeight);
+      console.log("isDesktop: ", isDesktop);
     };
 
     // コンポーネントがマウントされたときに初回実行
@@ -32,7 +37,8 @@ export function TestResponsiveHome() {
     };
   }, []); // 空の依存配列を渡して初回のみ実行されるようにする
 
-  const isDesktop: boolean = useMediaQuery({ query: '(min-width: 768px)' })
+  //const isDesktop: boolean = useMediaQuery({ query: '(min-width: 768px)' })
+  console.log("isDesktop: ", isDesktop);
 
   return (
     <div>
