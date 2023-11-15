22bb0d693aa673a12c49c3af42f5fe5f387c92c3
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-15 15:44:05 +0900

    ウィンドウのサイズを取得する機能の初期実装

--- src/views/TestResponsiveHome.tsx
+++ src/views/TestResponsiveHome.tsx
@@ -1,5 +1,5 @@
 import '../App.css'
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
 import Template from '../pages/template';
 import MediaQuery from "react-responsive";
@@ -8,6 +8,29 @@ import TestPageForDesktopComponent from '../pages/TestPageForDesktopComponet';
 import TestPageForMobileComponent from '../pages/TestPageForMobileComponent';
 
 export function TestResponsiveHome() {
+
+  useEffect(() => {
+    // ウィンドウのサイズが変更されたときに実行されるハンドラーを登録
+    const handleResize = () => {
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+
+      // ここで取得した windowWidth と windowHeight を使って必要な処理を行う
+      console.log('Window Width:', windowWidth);
+      console.log('Window Height:', windowHeight);
+    };
+
+    // コンポーネントがマウントされたときに初回実行
+    handleResize();
+
+    // ウィンドウのリサイズイベントにハンドラーを追加
+    window.addEventListener('resize', handleResize);
+
+    // コンポーネントがアンマウントされたときにハンドラーをクリーンアップ
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // 空の依存配列を渡して初回のみ実行されるようにする
 
   const isDesktop: boolean = useMediaQuery({ query: '(min-width: 768px)' })
 
