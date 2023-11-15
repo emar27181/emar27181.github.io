2a9c187b240ca95b0e45a321ae1756784c429ff8
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-15 16:01:08 +0900

    キャンバスをレスポンシブデザインで表示する機能の初期実装

---
+++ src/pages/CanvasForMobileComponent.tsx
@@ -0,0 +1,31 @@
+import '../App.css'
+import React from 'react';
+import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
+import BounceColorful from '../pages/BounceColorful';
+import DisplayEmotionColorRatio from '../pages/Reserch/DisplayEmotionColorRatio';
+import ExplainPluticCircle from '../pages/Reserch/ExplainPluticCircle';
+import Canvas from '../pages/Reserch/Canvas';
+import ColorGanerate from '../pages/ColorRecommendation/ColorGenerate';
+import FormDisplay from '../pages/Reserch/FormDisplay';
+import SendData from '../pages/Reserch/SendData';
+import ReceiveData from '../pages/Reserch/ReceiveData';
+import DisplayInfo from '../pages/Reserch/DisplayInfo';
+import ExplainPluticCircleEnglish from '../pages/Reserch/ExplainPluticCircleEnglish';
+import ExplainDrawing from '../pages/Reserch/ExplainDrawing';
+import EmotionAnalyzeButton from '../pages/Reserch/AnalyzeEmotionRequest';
+import DisplayDrawingInfo from '../pages/Reserch/DisplayDrawingInfo';
+import GravityBall from '../pages/GravityBall';
+import ReturnCameraInfo from '../pages/Reserch/ReturnCameraInfo';
+import DisplayGravityPlace from '../pages/Reserch/DisplayGravityPlace';
+import ReturnImageInfo from '../pages/Reserch/ReturnImageInfo';
+
+export function CanvasForMobileComponent() {
+  return (
+    <div>
+      <Canvas />
+      <DisplayGravityPlace />
+    </div>
+  )
+}
+
+export default CanvasForMobileComponent
