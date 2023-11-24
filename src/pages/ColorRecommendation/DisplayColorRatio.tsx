0f70f733cf853b5cb2fc9d2dedb021d0dca5a892
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-25 01:34:58 +0900

    jsonデータを読み込んでその色の数だけ表示させる機能の実装

--- src/pages/ColorRecommendation/DisplayColorRatio.tsx
+++ src/pages/ColorRecommendation/DisplayColorRatio.tsx
@@ -14,15 +14,27 @@ export function DisplayColorRatio() {
     p.setup = () => {
       let rate = 0.65;
       p.createCanvas(rate * p.windowWidth / 2, 20);
-      p.createCanvas(CANVAS_SIZE[0], 20);
+      //p.createCanvas(CANVAS_SIZE[0], 20);
       p.background(0);
+      p.noStroke();
+      displayColors();
+      
     };
 
 
     p.draw = () => {
-      p.colorMode(p.HSL, 360, 100, 100);
-      p.background(hue, saturation, lightness);
+      //p.colorMode(p.HSL, 360, 100, 100);
+      //p.background(hue, saturation, lightness);
     };
+
+    function displayColors() {
+      for (let i = 0; i < jsonData.length; i++) {
+        p.colorMode(p.HSL, 360, 100, 100);
+        p.fill(ColorTestData[i].hue, ColorTestData[i].saturation, ColorTestData[i].lightness);
+        p.rect(p.width / jsonData.length * i, 0, p.width / jsonData.length, p.height);
+      }
+    }
+
   }
 
   return (
