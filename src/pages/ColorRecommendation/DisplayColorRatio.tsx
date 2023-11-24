c0f024566dbaa34bdf3d7c63bc13cf1f2aa8ff1a
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-25 00:50:59 +0900

    jsonデータを読み込んで色を表示させる機能の初期実装

--- src/pages/ColorRecommendation/DisplayColorRatio.tsx
+++ src/pages/ColorRecommendation/DisplayColorRatio.tsx
@@ -2,18 +2,26 @@ import '../../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
 import { ReturnCanvasSize } from '../Reserch/Canvas';
+import ColorTestData from '../../data/colorTestData.json'
 
 export function DisplayColorRatio() {
   const sketch = (p: P5CanvasInstance) => {
 
     const CANVAS_SIZE = ReturnCanvasSize();
-    
+    let jsonData = ColorTestData;
+    let hue = jsonData[0].hue, saturation = jsonData[0].saturation, lightness = jsonData[0].lightness;
+
     p.setup = () => {
+      let rate = 0.65;
+      p.createCanvas(rate * p.windowWidth / 2, 20);
       p.createCanvas(CANVAS_SIZE[0], 20);
-      p.background(0); 
+      p.background(0);
     };
 
+
     p.draw = () => {
+      p.colorMode(p.HSL, 360, 100, 100);
+      p.background(hue, saturation, lightness);
     };
   }
 
