780a1d475d473708f3733a972282d5b1cfe4e455
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-26 00:20:01 +0900

    バックエンドから取得した色の組み合わせを表示させる機能の初期実装

--- src/pages/ColorRecommendation/DisplayColorRatio.tsx
+++ src/pages/ColorRecommendation/DisplayColorRatio.tsx
@@ -3,17 +3,19 @@ import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
 import { ReturnCanvasSize } from '../Reserch/Canvas';
 import ColorTestData from '../../data/colorTestData.json';
+import * as fs from 'fs';
+import axios from 'axios';
 
-
+//let jsonFilePath = '../../data/colorTestData.json';
+let jsonFilePath = 'src/data/colorTestData.json';
 let isTouchedColorRatio = false;
 let red = 0, green = 0, blue = 0, alpha = 0;
+let hue: number[] = [], saturation: number[] = [], lightness: number[] = [];
+fetchData();
 
 export function DisplayColorRatio() {
   const sketch = (p: P5CanvasInstance) => {
-
     const CANVAS_SIZE = ReturnCanvasSize();
-    let jsonData = ColorTestData;
-    let hue = jsonData[0].hue, saturation = jsonData[0].saturation, lightness = jsonData[0].lightness;
 
     p.setup = () => {
       let rate = 0.65;
@@ -22,7 +24,7 @@ export function DisplayColorRatio() {
       p.background(0);
       p.noStroke();
       displayColors();
-
+      console.log(hue[0])
     };
 
 
@@ -33,12 +35,70 @@ export function DisplayColorRatio() {
       if (p.mouseIsPressed) { mouseControl(); }
     };
 
+    function editJson() {
+      // JSON ファイルを読み込む
+      //const rawData = fs.readFileSync(jsonFilePath, 'utf-8');
+      //const jsonData = JSON.parse(rawData);
+      //console.log(jsonData);
+      // JSONファイルをフェッチする
+
+      // JSONファイルの読み込み
+      /*
+      const readJsonFile = (filePath: string): any => {
+        try {
+          const data = fs.readFileSync(filePath, 'utf-8');
+          return JSON.parse(data);
+        } catch (error) {
+          console.error('Error reading JSON file:', error);
+          return null;
+        }
+      };
+
+      // JSONファイルの書き込み
+      const writeJsonFile = (filePath: string, data: any): void => {
+        try {
+          const jsonData = JSON.stringify(data, null, 2);
+          fs.writeFileSync(filePath, jsonData, 'utf-8');
+          console.log('JSON file written successfully.');
+        } catch (error) {
+          console.error('Error writing JSON file:', error);
+        }
+      };
+
+      const jsonData = readJsonFile(jsonFilePath);
+      jsonData[0].hue = 45;
+      writeJsonFile(jsonFilePath, jsonData);
+      */
+
+
+      fetch(jsonFilePath)
+        .then(response => response.json())
+        .then(jsonData => {
+          // JSONデータを変更する（例： "name"プロパティを変更）
+          jsonData[0].hue = 45;
+          console.log(jsonData);
+        })
+        .catch(error => console.error('JSONの読み込みエラー:', error));
+
+
+      //fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2), 'utf-8');
+
+    }
+
+
     function displayColors() {
+      for (let i = 0; i < hue.length; i++) {
+        p.colorMode(p.HSL);
+        p.fill(hue[i], saturation[i], lightness[i]);
+        p.rect(p.width / hue.length * i, 0, p.width / hue.length, p.height);
+      }
+      /*
       for (let i = 0; i < jsonData.length; i++) {
         p.colorMode(p.HSL, 360, 100, 100);
         p.fill(ColorTestData[i].hue, ColorTestData[i].saturation, ColorTestData[i].lightness);
         p.rect(p.width / jsonData.length * i, 0, p.width / jsonData.length, p.height);
       }
+      */
     }
 
     function mouseControl() {
@@ -54,12 +114,29 @@ export function DisplayColorRatio() {
         // console.log(red, green, blue, alpha);
       }
     }
-
   }
 
   return (
     <ReactP5Wrapper sketch={sketch} />
   )
+}
+
+async function fetchData() {
+  try {
+    const response = await axios.get('http://localhost:5000/api/send-color-combination-data');
+    const jsonData = response.data;
+    const parsedJsonData = JSON.parse(jsonData);
+
+    for (let i = 0; i < parsedJsonData.length; i++) {
+      let data = parsedJsonData[i];
+      hue[i] = data.hue;
+      saturation[i] = data.saturation;
+      lightness[i] = data.lightness;
+      //console.log("hue[" + i + "]: " + hue[i] + ", saturation[" + i + "]: " + saturation[i] + ", lightness[" + i + "]: " + lightness[i]);
+    }
+  } catch (error) {
+    console.error('エラーが発生しました:', error);
+  }
 }
 
 export function ReturnColorRatioValue() {
