d2de8ecf5b0142bb1319e59e81152851404077ff
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-26 20:12:10 +0900

    描画色を基に色の組み合わせを表示する機能の初期実装

--- src/pages/ColorRecommendation/DisplayColorRatioOnlyFrontendontend.tsx
+++ src/pages/ColorRecommendation/DisplayColorRatioOnlyFrontendontend.tsx
@@ -15,12 +15,14 @@ import p5 from 'p5';
 let jsonFilePath = 'src/data/colorTestData.json';
 let isTouchedColorRatio = false;
 let red = 0, green = 0, blue = 0, alpha = 0;
-let hue: number[] = [], saturation: number[] = [], lightness: number[] = [];
+//let hue: number[] = [], saturation: number[] = [], lightness: number[] = [];
 
 export function DisplayColorRatioOnlyFrontendontend() {
   const sketch = (p: P5CanvasInstance) => {
     const CANVAS_SIZE = ReturnCanvasSize();
     let drawingColor: p5.Color = ReturnDrawingColor();
+    let colors: p5.Color[] = [];
+    let colorsNumber = 5;
 
     p.setup = () => {
       let rate = 0.65;
@@ -34,28 +36,44 @@ export function DisplayColorRatioOnlyFrontendontend() {
 
     p.draw = () => {
       p.colorMode(p.HSL, 360, 100, 100);
-      UpdateVariables();
+      updateVariables();
       displayColors();
       if (p.mouseIsPressed) { mouseControl(); }
     };
 
-    function UpdateVariables() {
+    function updateVariables() {
       drawingColor = ReturnDrawingColor();
+      caluculateColors();
       isTouchedColorRatio = false;
     }
 
-    function displayColors() {
+    function caluculateColors() {
+      p.colorMode(p.HSL);
+      colors[0] = (drawingColor);
+      let hue = p.hue(drawingColor);
+      let saturation = p.saturation(drawingColor);
+      let lightness = p.lightness(drawingColor);
 
-      p.fill(drawingColor);
-      p.rect(0, 0, p.width, p.height);
-
-      /*
-      for (let i = 0; i < hue.length; i++) {
-        p.colorMode(p.HSL);
-        p.fill(hue[i], saturation[i], lightness[i]);
-        p.rect(p.width / hue.length * i, 0, p.width / hue.length, p.height);
+      for (let i = 1; i < colorsNumber; i++) {
+        // 初期実装として同一色相配色を実装
+        saturation -= 20;
+        lightness -= 20;
+        colors[i] = p.color(hue, saturation, lightness);
+        console.log(colors[i]);
       }
-      */
+
+    }
+
+    function displayColors() {
+      p.colorMode(p.HSL);
+      //p.fill(drawingColor);
+      //p.rect(0, 0, p.width / (colors.length), p.height);
+
+      for (let i = 0; i < colors.length; i++) {
+        p.fill(colors[i]);
+        p.rect(p.width / colors.length * i, 0, p.width / colors.length, p.height);
+      }
+
     }
 
     function mouseControl() {
