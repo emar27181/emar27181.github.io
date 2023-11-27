b99e21ebc95d56cd794b2c3f3ee6cba7809f9883
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-27 15:56:18 +0900

    ログデータの読み込みの初期実装

--- src/pages/DisplayMatchBoLog.tsx
+++ src/pages/DisplayMatchBoLog.tsx
@@ -1,20 +1,22 @@
 import '../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
+import matchBoLog from '../data/matchBoTestLog.json'
 
 export function DisplayMatchBoLog() {
   const sketch = (p: P5CanvasInstance) => {
 
     const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
-    
+
     p.setup = () => {
       p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-      p.background(0); 
+      p.background(0);
+      console.log(matchBoLog);
     };
 
     p.draw = () => {
-      p.fill(255); 
-      p.ellipse(p.width / 2, p.height / 2, 100, 100); 
+      p.fill(255);
+      p.ellipse(p.width / 2, p.height / 2, 100, 100);
     };
   }
 
