987e937a386dc13547f23c32256deb5a111ef727
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-28 18:40:49 +0900

    色の推薦ページに色の割合を表示する画面を表示

--- src/views/ColorRecommendHome.tsx
+++ src/views/ColorRecommendHome.tsx
@@ -7,6 +7,8 @@ import Canvas from '../pages/Reserch/Canvas';
 import DisplayDrawingInfo from '../pages/Reserch/DisplayDrawingInfo';
 import DisplayColorRatioOnlyFrontendontend from '../pages/ColorRecommendation/DisplayColorRatioOnlyFrontendontend';
 import DisplayUsedColorRatio from '../pages/ColorRecommendation/DisplayUsedColorRatio';
+import ReturnCameraInfo from '../pages/Reserch/ReturnCameraInfo';
+import ReturnImageInfo from '../pages/Reserch/ReturnImageInfo';
 
 export function ColorRecommendHome() {
 
@@ -14,6 +16,15 @@ export function ColorRecommendHome() {
   return (
     <div>
       <br /><br /><br /><br />
+
+      <div style={{ display: 'flex' }}>
+        <ReturnCameraInfo />
+        <DisplayUsedColorRatio />
+      </div>
+      <div style={{ display: 'flex' }}>
+        <ReturnImageInfo />
+        <DisplayUsedColorRatio />
+      </div>
       <div style={{ display: 'flex' }}>
         <ColorGanerate />
         <DisplayColorRatioOnlyFrontendontend />
