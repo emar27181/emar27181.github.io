8c070e0f936934f5b26f3827fec803dcc309f117
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-11-30 18:16:18 +0900

    複数レイヤーを管理する機能の初期実装

--- src/pages/TestMultipleLayer.tsx
+++ src/pages/TestMultipleLayer.tsx
@@ -1,21 +1,35 @@
 import '../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
+import { Graphics } from 'p5';
 
 export function TestMultipleLayer() {
   const sketch = (p: P5CanvasInstance) => {
 
     const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
+    let mainCanvas: Graphics;
+    let additionalLayer: Graphics;
 
     p.setup = () => {
-      p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-      p.background(0);
-    };
+      p.createCanvas(400, 400);
+
+      mainCanvas = p.createGraphics(p.width, p.height);
+      additionalLayer = p.createGraphics(p.width, p.height);
+    }
 
     p.draw = () => {
-      p.fill(255);
-      p.ellipse(p.width / 2, p.height / 2, 100, 100);
-    };
+      // メインのキャンバスに描画
+      mainCanvas.background(220);
+      mainCanvas.ellipse(p.mouseX, p.mouseY, 50, 50);
+
+      // 追加のレイヤーに描画
+      additionalLayer.background(255, 0, 0, 1);
+      additionalLayer.rect(50, 50, 100, 100);
+
+      // メインのキャンバスに追加のレイヤーを合成
+      p.image(mainCanvas, 0, 0);
+      p.image(additionalLayer, 0, 0);
+    }
   }
 
   return (
