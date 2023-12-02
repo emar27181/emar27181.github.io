33d36ea6e867b64f9a208d00ef421e527025bb83
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-12-03 00:29:44 +0900

    読み込んだ画像をp5jsで表示させる初期実装

--- src/pages/TestDragAndPaste.tsx
+++ src/pages/TestDragAndPaste.tsx
@@ -1,10 +1,30 @@
 import '../App.css';
 import React, { useCallback } from 'react';
+import p5 from 'p5'; // p5.jsのライブラリをインポート
 import { useDropzone } from 'react-dropzone';
 
 export function TestDragAndPaste() {
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    console.log(acceptedFiles); // ドロップされたファイルの情報を出力
+    console.log(acceptedFiles);
+
+    // p5.jsのスケッチを作成
+    const sketch = (p: any) => {
+      let img: p5.Image;
+
+      p.preload = () => {
+        // 画像の読み込み
+        img = p.loadImage(URL.createObjectURL(acceptedFiles[0]));
+      };
+
+      p.setup = () => {
+        p.createCanvas(400, 400);
+        // 読み込んだ画像を表示
+        p.image(img, 0, 0, p.width, p.height);
+      };
+    };
+
+    // p5インスタンスを作成
+    const myp5 = new p5(sketch);
   }, []);
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
