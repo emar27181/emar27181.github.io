293d5bd9324a62c074193d1ff316685687ef79ff
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-12-06 18:23:29 +0900

    読み込まれた画像を基にキャンバス上に表示させる機能の実装

--- src/pages/ColorRecommendation/DisplayCanvasFilter.tsx
+++ src/pages/ColorRecommendation/DisplayCanvasFilter.tsx
@@ -1,13 +1,13 @@
 import '../../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
-import { ReturnCanvasColors } from '../Reserch/Canvas';
+import { ReturnCanvasColors, ReturnCanvasSize } from '../Reserch/Canvas';
 import React from 'react';
 import p5 from 'p5';
 
 export function DisplayCanvasFilter() {
   const sketch = (p: P5CanvasInstance) => {
     const SPLIT = 100;
-
+    let canvasWidth = 0, canvasHeight = 0;
     let canvasColors: p5.Color[][] = [];
     for (let i = 0; i < SPLIT; i++) { canvasColors[i] = []; }
     for (let i = 0; i < SPLIT; i++) {
@@ -18,7 +18,8 @@ export function DisplayCanvasFilter() {
 
     p.setup = () => {
       p.createCanvas(200, 200);
-      p.background(0);
+      p.background(255);
+      p.noStroke();
     };
 
     p.draw = () => {
@@ -28,15 +29,18 @@ export function DisplayCanvasFilter() {
 
     function updateVariables() {
       canvasColors = ReturnCanvasColors();
+      let canvasSize = ReturnCanvasSize();
+      canvasWidth = canvasSize[0];
+      canvasHeight = canvasSize[1];
     }
 
     function displayCanvas() {
 
       for (let i = 0; i < SPLIT; i++) {
         for (let j = 0; j < SPLIT; j++) {
-          //canvasColors[i][j] = p.color(0, 0, 0);
           p.fill(canvasColors[i][j]);
-          p.rect(i, j, p.width / SPLIT, p.height / SPLIT);
+          p.rect(p.width / SPLIT * i, p.height / SPLIT * j, p.width / SPLIT, p.height / SPLIT);
+          //p.rect((canvasWidth / SPLIT * i) * (p.width / canvasWidth), (canvasHeight / SPLIT * j) * (p.width / canvasHeight), p.width / SPLIT, p.height / SPLIT);
         }
       }
     }
