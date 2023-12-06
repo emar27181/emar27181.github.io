6c8a5c18412a2ace2cf49a72404a425ce8289dc0
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-12-06 19:20:48 +0900

    背景色を除外して表示させる機能の実装

--- src/pages/ColorRecommendation/DisplayCanvasFilter.tsx
+++ src/pages/ColorRecommendation/DisplayCanvasFilter.tsx
@@ -7,8 +7,10 @@ import p5 from 'p5';
 export function DisplayCanvasFilter(displayMode: string) {
   const sketch = (p: P5CanvasInstance) => {
     const SPLIT = 100;
+    const SATURATION_LIMIT = 10;
     let canvasWidth = 0, canvasHeight = 0;
     let canvasColors: p5.Color[][] = [];
+    let backgroundColor = p.color(255, 255, 255);
     for (let i = 0; i < SPLIT; i++) { canvasColors[i] = []; }
     for (let i = 0; i < SPLIT; i++) {
       for (let j = 0; j < SPLIT; j++) {
@@ -18,7 +20,7 @@ export function DisplayCanvasFilter(displayMode: string) {
 
     p.setup = () => {
       p.createCanvas(200, 200);
-      p.background(255);
+      p.background(backgroundColor);
       p.noStroke();
     };
 
@@ -39,22 +41,36 @@ export function DisplayCanvasFilter(displayMode: string) {
       for (let i = 0; i < SPLIT; i++) {
         for (let j = 0; j < SPLIT; j++) {
           setColor(canvasColors[i][j]);
+          //console.log("(" + p.red(canvasColors[i][j]) + ", " + p.green(canvasColors[i][j]) + ", " + p.blue(canvasColors[i][j]) + ")");
           p.rect(p.width / SPLIT * i, p.height / SPLIT * j, p.width / SPLIT, p.height / SPLIT);
         }
       }
     }
 
     function setColor(color: p5.Color): void {
-      p.colorMode(p.HSB);
-      if (displayMode === "hue") {
+      //if (p.saturation(color) <= SATURATION_LIMIT) {
+      if (equalsColor(color, backgroundColor)) {
+        p.fill(backgroundColor);
+      }
+      else if (displayMode === "hue") {
+        p.colorMode(p.HSL);
         p.fill(p.hue(color), 70, 70);
       }
       else if (displayMode === "saturation") {
+        p.colorMode(p.HSB);
         p.fill(0, p.saturation(color), p.saturation(color));
       }
       else if (displayMode === "lightness") {
-        p.fill(0, 0, p.brightness(color));
+        p.colorMode(p.HSL);
+        p.fill(0, 0, p.lightness(color));
       }
+    }
+
+    function equalsColor(color1: p5.Color, color2: p5.Color) {
+      let red1 = p.red(color1), green1 = p.green(color1), blue1 = p.blue(color1);
+      let red2 = p.red(color2), green2 = p.green(color2), blue2 = p.blue(color2);
+      //console.log("equalsColor((" + red1 + ", " + green1 + ", " + blue1 + "), (" + red2 + ", " + green2 + ", " + blue2 + "))= " + ((red1 === red2) && (green1 === green2) && (blue1 === blue2)));
+      return (((red1 === red2) && (green1 === green2) && (blue1 === blue2)));
     }
   }
 
