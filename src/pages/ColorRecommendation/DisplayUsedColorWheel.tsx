fea281fa9c1c762b267e0702fe9ec6bce564096c
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-12-13 14:58:41 +0900

    使われた色相が表示されるように変更

--- src/pages/ColorRecommendation/DisplayUsedColorWheel.tsx
+++ src/pages/ColorRecommendation/DisplayUsedColorWheel.tsx
@@ -1,20 +1,48 @@
 import '../../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
+import { ColorAmount, ReturnColorsAmount } from './DisplayUsedColorRatio';
 
 export function DisplayUsedColorWheel() {
   const sketch = (p: P5CanvasInstance) => {
+
+    let colorsAmount: Array<ColorAmount> = [];
+    let radius = 0;
 
     p.setup = () => {
       let rate = 0.35;
       p.createCanvas(rate * window.innerWidth / 3, rate * window.innerWidth / 3);
       p.noStroke();
+      radius = 3 / 8 * p.width;
     }
 
     p.draw = () => {
+      updateVariables();
       p.background(0);
       p.translate(p.width / 2, p.height / 2);
-      drawColorWheel(3 / 8 * p.width, 1);
+
+      drawColorWheel(radius, 15);
+      drawUsedColorHue();
+    }
+
+    function updateVariables() {
+      colorsAmount = ReturnColorsAmount();
+    }
+
+    function drawUsedColorHue() {
+      let SATURATION_LIMIT = 15;
+      for (let i = 0; i < colorsAmount.length; i++) {
+        if (colorsAmount[i].amount <= 20 || p.saturation(colorsAmount[i].color) <= SATURATION_LIMIT) { continue; }
+        let angle = p.hue(colorsAmount[i].color);
+        let x = radius * p.cos(p.radians(angle));
+        let y = radius * p.sin(p.radians(angle));
+
+        p.noFill();
+        p.stroke(255);
+        p.strokeWeight(0.005 * p.width);
+        p.ellipse(x, y, 0.03 * p.width);
+
+      }
     }
 
     function drawColorWheel(radius: number, resolution: number) {
