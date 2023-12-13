53920ed0b1610dec177a1387f5d0e3569101347b
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-12-13 14:39:03 +0900

    色相環を表示させる機能の実装

--- src/pages/ColorRecommendation/DisplayUsedColorWheel.tsx
+++ src/pages/ColorRecommendation/DisplayUsedColorWheel.tsx
@@ -5,17 +5,36 @@ import React from 'react';
 export function DisplayUsedColorWheel() {
   const sketch = (p: P5CanvasInstance) => {
 
-
     p.setup = () => {
       let rate = 0.35;
       p.createCanvas(rate * window.innerWidth / 3, rate * window.innerWidth / 3);
-      p.background(0);
-    };
+      p.noStroke();
+    }
 
     p.draw = () => {
-      p.fill(255);
-      p.ellipse(p.width / 2, p.height / 2, 100, 100);
-    };
+      p.background(0);
+      p.translate(p.width / 2, p.height / 2);
+      drawColorWheel(3 / 8 * p.width, 1);
+    }
+
+    function drawColorWheel(radius: number, resolution: number) {
+      for (let angle = 0; angle < 360; angle += resolution) {
+        let hue = angle;
+        let color = p.color('hsb(' + hue + ', 100%, 100%)');
+
+        // 極座標から直交座標に変換
+        let x = radius * p.cos(p.radians(angle));
+        let y = radius * p.sin(p.radians(angle));
+
+        // 中心から色相環への線を描画
+        p.stroke(color);
+        p.line(0, 0, x, y);
+
+        // 色相環の点を描画
+        p.fill(color);
+        p.ellipse(x, y, p.width / 40, p.height / 40);
+      }
+    }
   }
 
   return (
