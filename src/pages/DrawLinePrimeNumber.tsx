c837fcec269939ba32e50a02df424cd0099bd5d7
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-12-27 19:19:37 +0900

    素数フレームで左に回転する機能の初期実装

--- src/pages/DrawLinePrimeNumber.tsx
+++ src/pages/DrawLinePrimeNumber.tsx
@@ -6,16 +6,39 @@ export function DrawLinePrimeNumber() {
   const sketch = (p: P5CanvasInstance) => {
 
     const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
-    
+    let positions: Array<Position> = [];
+
     p.setup = () => {
-      p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-      p.background(0); 
+      p.createCanvas(window.innerWidth / 3, window.innerWidth / 3);
+      //p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+      p.background(0);
+
+      positions.push(new Position(0, 0));
     };
 
     p.draw = () => {
-      p.fill(255); 
-      p.ellipse(p.width / 2, p.height / 2, 100, 100); 
+      p.translate(p.width / 2, p.height / 2);
+      p.fill(255);
+
+
+      drawLine();
     };
+
+    function drawLine() {
+      for (let i = 0; i < positions.length; i++) {
+        p.ellipse(positions[i].x++, positions[i].y++, 3);
+      }
+    }
+
+    class Position {
+      x: number;
+      y: number;
+
+      constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+      }
+    }
   }
 
   return (
