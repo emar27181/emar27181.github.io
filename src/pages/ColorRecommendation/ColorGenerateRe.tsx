ed932e235ac0b0ed2122a3ec3018aa0a5a6788a6
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2023-12-27 17:30:25 +0900

    トーン表で色相が分かるように変更

--- src/pages/ColorRecommendation/ColorGenerateRe.tsx
+++ src/pages/ColorRecommendation/ColorGenerateRe.tsx
@@ -44,7 +44,7 @@ export function ColorGanerateRe() {
       displayDrawingColor();
       displayDrawingColorInfo();
 
-      //drawLimitLine();
+      drawLimitLine();
     };
 
     function drawLimitLine() {
@@ -60,16 +60,16 @@ export function ColorGanerateRe() {
     function displayUsedColorsDot() {
       //const SATURATION_LIMIT = 0;
       for (let i = 0; i < colorsAmount.length; i++) {
-        /*
+
         if (p.saturation(colorsAmount[i].color) <= SATURATION_LIMIT) {
           continue;
         }
-        */
-        if (colorsAmount[i].amount >= 30) {
-          //if (colorsAmount[i].amount >= AMOUNT_LIMIT) {
+
+        if (colorsAmount[i].amount >= AMOUNT_LIMIT) {
           let displayRate = 0.005 * colorsAmount[i].amount;
-          displayColorsDot(colorsAmount[i].color, 1, p.color(0),);
-          //displayColorsDot(colorsAmount[i].color, 1, p.color(p.hue(colorsAmount[i].color), 50, 50));
+          //displayColorsDot(colorsAmount[i].color, 1, p.color(0),);
+          displayColorsDot(colorsAmount[i].color, 1, p.color(p.hue(colorsAmount[i].color), 50, 50));
+          //displayColorsDot(colorsAmount[i].color, 1, p.color(colorsAmount[i].color));
         }
       }
     }
@@ -140,16 +140,17 @@ export function ColorGanerateRe() {
       }
     }
 
-    function displayColorsDot(color: p5.Color, displayRate: number, strokeColor: p5.Color) {
+    function displayColorsDot(color: p5.Color, displayRate: number, fillColor: p5.Color) {
       p.colorMode(p.HSL);
       let saturation = p.round(p.saturation(color));
       let brightness = p.round(p.brightness(color));
       let lightness = p.round(p.lightness(color));
 
-      p.noFill();
-      p.stroke(strokeColor);
+      p.fill(fillColor);
+      p.noStroke();
       p.strokeWeight(0.005 * p.width);
-      p.ellipse(saturation / 100 * p.width, p.width - lightness / 100 * p.width, p.width / SPLIT + displayRate * 0.03 * p.width);
+      p.ellipse(saturation / 100 * p.width, p.width - lightness / 100 * p.width, p.width / SPLIT + displayRate * 0.01 * p.width);
+      //p.ellipse(saturation / 100 * p.width, p.width - lightness / 100 * p.width, p.width / SPLIT + displayRate * 0.03 * p.width);
       //p.rect(brightness / 100 * p.width, saturation / 100 * p.width, p.width / SPLIT);
     }
 
