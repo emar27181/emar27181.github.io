6f6ef6933c1804874a5c8211d3c4009165d9eaa7
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-01-12 15:07:56 +0900

    描画色を保存する変数の実装

--- src/pages/Reserch/CanvasOnlyDraw.tsx
+++ src/pages/Reserch/CanvasOnlyDraw.tsx
@@ -2,14 +2,16 @@ import '../../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
 import { DISPLAY_RATE } from '../../config/constants';
+import p5 from 'p5';
 
-let drawingWeight = 30;
+export let drawingWeight = 30;
+export let drawingColor: p5.Color;
 
 export function CanvasOnlyDraw() {
   const sketch = (p: P5CanvasInstance) => {
 
-
     p.setup = () => {
+      initializeVariables();
       p.createCanvas(DISPLAY_RATE * window.innerWidth, DISPLAY_RATE * window.innerWidth);
       p.background(255);
       p.frameRate(60);
@@ -19,6 +21,10 @@ export function CanvasOnlyDraw() {
       if (p.mouseIsPressed) { mousePressed(); }
     };
 
+    function initializeVariables() {
+      drawingColor = p.color(255, 0, 0);
+    }
+
     p.keyPressed = () => {
       if (p.key === "+") { drawingWeight += 0.2 * p.frameRate(); }
       else if (p.key === '-') { drawingWeight -= 0.2 * p.frameRate(); }
@@ -26,7 +32,7 @@ export function CanvasOnlyDraw() {
 
     function mousePressed() {
       p.noStroke();
-      p.fill(0);
+      p.fill(drawingColor);
       p.ellipse(p.mouseX, p.mouseY, drawingWeight);
     }
   }
