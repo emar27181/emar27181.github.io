6d1130ec3311d767917bcf25b71d3f7917479ca8
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-01-12 15:03:19 +0900

    色の推薦に関する機能の仮実装

--- src/views/CanvasHomeRe.tsx
+++ src/views/CanvasHomeRe.tsx
@@ -1,12 +1,20 @@
 import '../App.css'
 import React from 'react';
 import CanvasOnlyDraw from '../pages/Reserch/CanvasOnlyDraw';
+import ColorGanerateRe from '../pages/ColorRecommendation/ColorGenerateRe';
+import DisplayUsedColorWheel from '../pages/ColorRecommendation/DisplayUsedColorWheel';
+import OperateGuiControl from '../pages/Reserch/OperateGuiControl';
 
 export function CanvasHomeRe() {
 
-
   return (
-    <div>
+
+    <div style={{ display: 'flex', margin: 0 }}>
+      <div>
+        <ColorGanerateRe />
+        <DisplayUsedColorWheel />
+        <OperateGuiControl />
+      </div>
       <CanvasOnlyDraw />
     </div>
   )
