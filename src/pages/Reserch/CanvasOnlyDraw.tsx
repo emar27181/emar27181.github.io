7f6accd2711ebefbd7223b6ecb6a598132f63aa6
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-01-17 18:32:16 +0900

    色塗りのテンプレート画像の追加

--- src/pages/Reserch/CanvasOnlyDraw.tsx
+++ src/pages/Reserch/CanvasOnlyDraw.tsx
@@ -2,23 +2,36 @@ import '../../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
 import { DISPLAY_RATE } from '../../config/constants';
-import p5 from 'p5';
+import p5, { Graphics } from 'p5';
 import { ReturnColorPaletteValue } from '../ColorRecommendation/ColorGenerateRe';
+import imageFilePath0 from '../../assets/coloring_sample_image.png';
 
-export let drawingWeight = 30;
+export let drawingWeight = 0.06 * window.innerWidth;
 export let drawingColor: p5.Color;
+let coloringImages: Array<p5.Image> = [];
+let drawingLayer: Graphics;
 
 export function CanvasOnlyDraw() {
   const sketch = (p: P5CanvasInstance) => {
 
+    p.preload = () => {
+      coloringImages.push(p.loadImage(imageFilePath0));
+    }
+
     p.setup = () => {
       initializeVariables();
       p.createCanvas(DISPLAY_RATE * window.innerWidth, DISPLAY_RATE * window.innerWidth);
+      drawingLayer = p.createGraphics(p.width, p.height);
+      drawingLayer.background(p.color(255));
       p.background(255);
       p.frameRate(60);
     };
 
     p.draw = () => {
+
+
+      p.image(drawingLayer, 0, 0);
+      p.image(coloringImages[0], 0, 0);
       updateVariables();
       if (p.mouseIsPressed) { mousePressed(); }
     };
@@ -38,9 +51,9 @@ export function CanvasOnlyDraw() {
     }
 
     function mousePressed() {
-      p.noStroke();
-      p.fill(drawingColor);
-      p.ellipse(p.mouseX, p.mouseY, drawingWeight);
+      drawingLayer.noStroke();
+      drawingLayer.fill(drawingColor);
+      drawingLayer.ellipse(p.mouseX, p.mouseY, drawingWeight);
     }
   }
 
