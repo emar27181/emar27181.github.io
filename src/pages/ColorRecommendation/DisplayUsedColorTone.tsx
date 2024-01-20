ac896299990092f1b638d384e41a6e8e2ea2fef1
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-01-20 21:28:41 +0900

    トーン表の位置に円を描画する機能の実装

--- src/pages/ColorRecommendation/DisplayUsedColorTone.tsx
+++ src/pages/ColorRecommendation/DisplayUsedColorTone.tsx
@@ -5,22 +5,51 @@ import React from 'react';
 export function DisplayUsedColorTone() {
   const sketch = (p: P5CanvasInstance) => {
 
-    const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
+    const CANVAS_WIDTH = 64, CANVAS_HEIGHT = 64;
 
     p.setup = () => {
       p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-      p.background(0);
+      p.background(255);
     };
 
     p.draw = () => {
-      p.fill(255);
-      p.ellipse(p.width / 2, p.height / 2, 100, 100);
+      drawCircles();
     };
-  }
 
+    function drawCircles() {
+      const CIRCLE_SIZE = 2 * p.width / 10;
+
+      p.noFill();
+      p.stroke(0);
+
+      p.ellipse(1 * p.width / 10, 1 * p.width / 10, CIRCLE_SIZE);
+      p.ellipse(1 * p.width / 10, 3 * p.width / 10, CIRCLE_SIZE);
+      p.ellipse(1 * p.width / 10, 5 * p.width / 10, CIRCLE_SIZE);
+      p.ellipse(1 * p.width / 10, 7 * p.width / 10, CIRCLE_SIZE);
+      p.ellipse(1 * p.width / 10, 9 * p.width / 10, CIRCLE_SIZE);
+
+      p.ellipse(3 * p.width / 10, 2 * p.width / 10, CIRCLE_SIZE);
+      p.ellipse(3 * p.width / 10, 4 * p.width / 10, CIRCLE_SIZE);
+      p.ellipse(3 * p.width / 10, 6 * p.width / 10, CIRCLE_SIZE);
+      p.ellipse(3 * p.width / 10, 8 * p.width / 10, CIRCLE_SIZE);
+
+      p.ellipse(5 * p.width / 10, 2 * p.width / 10, CIRCLE_SIZE);
+      p.ellipse(5 * p.width / 10, 4 * p.width / 10, CIRCLE_SIZE);
+      p.ellipse(5 * p.width / 10, 6 * p.width / 10, CIRCLE_SIZE);
+      p.ellipse(5 * p.width / 10, 8 * p.width / 10, CIRCLE_SIZE);
+
+      p.ellipse(7 * p.width / 10, 3 * p.width / 10, CIRCLE_SIZE);
+      p.ellipse(7 * p.width / 10, 5 * p.width / 10, CIRCLE_SIZE);
+      p.ellipse(7 * p.width / 10, 7 * p.width / 10, CIRCLE_SIZE);
+
+      p.ellipse(9 * p.width / 10, 5 * p.width / 10, CIRCLE_SIZE);
+    }
+
+  }
   return (
     <ReactP5Wrapper sketch={sketch} />
   )
+
 }
 
 export default DisplayUsedColorTone
