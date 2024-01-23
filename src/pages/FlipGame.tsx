b2963ab0a9baff3a312b25b4e4c0ee3b481c6142
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-01-23 16:24:53 +0900

    色の反転機能の初期実装

--- src/pages/FlipGame.tsx
+++ src/pages/FlipGame.tsx
@@ -2,22 +2,45 @@ import '../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
 
+const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
+const SPLIT = 5
+
 export function FlipGame() {
   const sketch = (p: P5CanvasInstance) => {
 
-    const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
-
     p.setup = () => {
       p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-      p.background(200);
+      p.background(0);
     };
 
     p.draw = () => {
       drawLines();
     };
 
+    p.mouseClicked = () => {
+      if (0 < p.mouseX && p.mouseX < p.width && 0 < p.mouseY && p.mouseY < p.height) {
+        flipColor();
+      }
+    }
+
+    function flipColor() {
+      for (let i = 0; i < SPLIT; i++) {
+        for (let j = 0; j < SPLIT; j++) {
+          let x1 = i * p.width / SPLIT;
+          let y1 = j * p.height / SPLIT;
+          let x2 = (i + 1) * p.width / SPLIT;
+          let y2 = (j + 1) * p.height / SPLIT;
+
+          if (x1 < p.mouseX && p.mouseX < x2 && y1 < p.mouseY && p.mouseY < y2) {
+            p.fill(255);
+            p.rect(x1, y1, p.width / SPLIT, p.height / SPLIT);
+            //console.log("(" + i + "," + j + "): = (" + p.round(x1) + "," + p.round(y1) + "," + p.round(x2) + "," + p.round(y2) + ")");
+          }
+        }
+      }
+    }
+
     function drawLines() {
-      const SPLIT = 3
       p.stroke(255);
       //格子状の線
       for (let i = 0; i < SPLIT; i++) {
