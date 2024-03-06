9978d72959c277c8d7429b895647f7a83f43c73c
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-03-06 15:09:02 +0900

    キャンバスサイズがウィンドウに合わせて動的に変化するように変更

--- src/pages/ColorRecommendation/DisplayRecommendColorsPalette.tsx
+++ src/pages/ColorRecommendation/DisplayRecommendColorsPalette.tsx
@@ -1,6 +1,7 @@
 import '../../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
+import { DISPLAY_RATE, DISPLAY_USED_COLOR_WHEEL_RATE } from '../../config/constants';
 
 export function DisplayRecommendColorsPalette() {
   const sketch = (p: P5CanvasInstance) => {
@@ -8,7 +9,8 @@ export function DisplayRecommendColorsPalette() {
     const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
 
     p.setup = () => {
-      p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+      //p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+      p.createCanvas(DISPLAY_RATE * DISPLAY_USED_COLOR_WHEEL_RATE * window.innerWidth / 3, DISPLAY_RATE * DISPLAY_USED_COLOR_WHEEL_RATE * window.innerWidth / 50);
       p.background(0);
     };
 
