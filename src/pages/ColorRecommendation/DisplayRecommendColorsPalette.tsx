1c89057e8d3392308e44896b3219bf57d6be72d1
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-03-06 22:11:05 +0900

    推定された配色と推薦された配色技法が表示されるように変更

--- src/pages/ColorRecommendation/DisplayRecommendColorsPalette.tsx
+++ src/pages/ColorRecommendation/DisplayRecommendColorsPalette.tsx
@@ -10,7 +10,7 @@ export function DisplayRecommendColorsPalette() {
     const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
 
     p.setup = () => {
-      p.createCanvas(DISPLAY_RATE * DISPLAY_USED_COLOR_WHEEL_RATE * window.innerWidth / 3, DISPLAY_RATE * DISPLAY_USED_COLOR_WHEEL_RATE * window.innerWidth / 50);
+      p.createCanvas(DISPLAY_RATE * DISPLAY_USED_COLOR_WHEEL_RATE * window.innerWidth / 3, DISPLAY_RATE * DISPLAY_USED_COLOR_WHEEL_RATE * window.innerWidth / 25);
       p.background(0);
       p.frameRate(1);
     };
@@ -18,16 +18,19 @@ export function DisplayRecommendColorsPalette() {
     p.draw = () => {
       //console.log(usedColorsHue);
       //console.log(RecommendedColorsHue);
-      //displayColorsHue(usedColorsHue);
       p.background(0);
-      displayColorsHue(RecommendedColorsHue);
+      displayColorsHue(usedColorsHue, 0, p.height / 2);
+      displayColorsHue(RecommendedColorsHue, p.height / 2, p.height);
+      p.strokeWeight(0.001 * window.innerWidth);
+      p.stroke(0);
+      p.line(0, p.height / 2, p.width, p.height / 2);
     };
 
 
     //色相順に使用色を表示させる関数
-    function displayColorsHue(ColorsHue: number[]) {
+    function displayColorsHue(ColorsHue: number[], y1: number, y2: number) {
       p.colorMode(p.HSL);
-      //p.noStroke();
+      p.noStroke();
       let x = 0;
       for (let i = 0; i < 360; i++) {
         for (let j = 0; j < ColorsHue.length; j++) {
@@ -35,7 +38,7 @@ export function DisplayRecommendColorsPalette() {
           if (p.round(ColorsHue[j]) === i) {
             p.fill(ColorsHue[j], 100, 50);
             //p.fill(0, 50, 50);
-            p.rect(x, 0, x + (p.width / ColorsHue.length), p.height);
+            p.rect(x, y1, x + (p.width / ColorsHue.length), y2);
             //p.rect(i * (p.width / ColorsHue.length), 0, (i + 1) * (p.width / ColorsHue.length), p.height);
             x += p.width / ColorsHue.length;
             //console.log(i);
