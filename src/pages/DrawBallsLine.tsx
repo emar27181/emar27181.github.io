3ede26e7453133d4ba0e3329b48be9337eef034c
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-04-11 23:16:01 +0900

    BallClassの初期実装

--- src/pages/DrawBallsLine.tsx
+++ src/pages/DrawBallsLine.tsx
@@ -1,21 +1,38 @@
 import '../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
+import Ball from '../utils/Ball';
 
 export function DrawBallsLine() {
   const sketch = (p: P5CanvasInstance) => {
 
     const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
+    let balls: Array<Ball> = [];
+
+
 
     p.setup = () => {
       p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
       p.background(0);
+      initializeVariables();
     };
 
     p.draw = () => {
       p.fill(255);
-      p.ellipse(p.width / 2, p.height / 2, 100, 100);
+      drawBalls();
     };
+
+    function drawBalls() {
+      for (let i = 0; i < balls.length; i++) {
+        p.ellipse(balls[i].x, balls[i].y, 10);
+      }
+    }
+
+    function initializeVariables() {
+      for (let i = 0; i < 10; i++) {
+        balls.push(new Ball(i * 3, i * 3));
+      }
+    }
   }
 
   return (
