7300d45f848644d1966e519c172bfd95f01c152e
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-04-22 17:23:05 +0900

    配色として有効色と判定されるグリッド数の閾値を調整(10->100)

--- src/config/constants.tsx
+++ src/config/constants.tsx
@@ -3,4 +3,4 @@ export const DISPLAY_CANVAS_FILTER_RATE = 0.7;
 export const DISPLAY_USED_COLOR_RATIO_RATE = 0;
 export const DISPLAY_USED_COLOR_WHEEL_RATE = 2;
 export const SPLIT = 100; // キャンバスを分割するグリッド数
-export const AMOUNT_LIMIT = 10; // 配色として有効色と判定されるグリッド数の閾値
+export const AMOUNT_LIMIT = 100; // 配色として有効色と判定されるグリッド数の閾値
