11abc1aafce4490d7efa69ccb32dcf23316e0071
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-04-24 18:07:35 +0900

    推薦する配色を保存する配列の初期実装(最も多い使用色を代入)

--- src/pages/ColorRecommendation/CalculateRecommendColors.tsx
+++ src/pages/ColorRecommendation/CalculateRecommendColors.tsx
@@ -2,8 +2,10 @@ import '../../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
 import { ColorAmount } from '../../utils/ColorAmount';
+import { ReturnUsedColorSchemeAmount } from './CalculateUsedColors';
 
 let recommendedColorSchemeAmount: Array<ColorAmount> = [];
+let usedColorSchemeAmount: Array<ColorAmount> = [];
 
 export function CalculateRecommendColors() {
   const sketch = (p: P5CanvasInstance) => {
@@ -22,8 +24,34 @@ export function CalculateRecommendColors() {
     };
 
     function updateVariables() {
-      recommendedColorSchemeAmount.push(new ColorAmount(p.color(255, 0, 0), 100));
+      usedColorSchemeAmount = ReturnUsedColorSchemeAmount();
+      calculateRecommendColorSchemeAmount();
     }
+
+    function calculateRecommendColorSchemeAmount() {
+      recommendedColorSchemeAmount = [];
+
+      // 初期実装としてusedColorSchemeで最も多い色を推薦
+      let sumAmount = calculateColorsAmount(usedColorSchemeAmount);
+      let maxColorAmountIndex = 0;
+      for (let i = 0; i < usedColorSchemeAmount.length; i++) {
+        if (usedColorSchemeAmount[i].amount > usedColorSchemeAmount[maxColorAmountIndex].amount) {
+          maxColorAmountIndex = i;
+        }
+      }
+
+      recommendedColorSchemeAmount.push(new ColorAmount(usedColorSchemeAmount[maxColorAmountIndex].color, usedColorSchemeAmount[maxColorAmountIndex].amount));
+
+    }
+
+    function calculateColorsAmount(colorAmount: ColorAmount[]) {
+      let sumAmount = 0;
+      for (let i = 0; i < colorAmount.length; i++) {
+        sumAmount += colorAmount[i].amount;
+      }
+      return sumAmount;
+    }
+
   }
 
   return (
