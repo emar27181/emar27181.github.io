232983cb4afe14d188623c8144556b02bbeafa2d
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-04-27 18:44:53 +0900

    明度と彩度の有効範囲の閾値を宣言

--- src/config/constants.tsx
+++ src/config/constants.tsx
@@ -4,3 +4,6 @@ export const DISPLAY_USED_COLOR_RATIO_RATE = 0;
 export const DISPLAY_USED_COLOR_WHEEL_RATE = 2;
 export const SPLIT = 100; // キャンバスを分割するグリッド数
 export const AMOUNT_LIMIT = 100; // 配色として有効色と判定されるグリッド数の閾値
+export const SATURATION_LIMIT = 20;
+export const LIGHTNESS_UPPER_LIMIT = 80;
+export const LIGHTNESS_LOWER_LIMIT = 25;
