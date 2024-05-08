8cbf6f95d582c974ff846830477a6741319fd4d8
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-05-08 14:02:01 +0900

    有効範囲の閾値の調整

--- src/config/constants.tsx
+++ src/config/constants.tsx
@@ -2,8 +2,9 @@ export const DISPLAY_RATE = 0.42;
 export const DISPLAY_CANVAS_FILTER_RATE = 0.7;
 export const DISPLAY_USED_COLOR_RATIO_RATE = 0;
 export const DISPLAY_USED_COLOR_WHEEL_RATE = 2;
+
 export const SPLIT = 100; // キャンバスを分割するグリッド数
 export const AMOUNT_LIMIT = 100; // 配色として有効色と判定されるグリッド数の閾値
-export const SATURATION_LIMIT = 20;
-export const LIGHTNESS_UPPER_LIMIT = 80;
-export const LIGHTNESS_LOWER_LIMIT = 25;
+export const SATURATION_LIMIT = 10;
+export const LIGHTNESS_UPPER_LIMIT = 90;
+export const LIGHTNESS_LOWER_LIMIT = 10;
