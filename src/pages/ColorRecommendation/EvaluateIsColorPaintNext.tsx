91c78340530bdcc77a9ffeacf13bcea10d9e6aed
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-15 20:15:12 +0900

    推薦に次に塗る色が含まれているかどうかを確認する関数の初期実装

---
+++ src/pages/ColorRecommendation/EvaluateIsColorPaintNext.tsx
@@ -0,0 +1,10 @@
+
+// used[i][j]に対する推薦recommend[][]にused[i][j+1](次の色)が含まれているかどうか確認する関数
+export function isColorPaintNext(): boolean {
+  return false;
+}
+
+// 生成された推薦する配色の評価をまとめて行う関数
+export function evaluateRecommendColorSchemes(): number {
+  return 0;
+}
