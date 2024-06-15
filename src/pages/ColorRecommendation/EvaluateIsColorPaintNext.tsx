d53992032aa68719e22cbec9db0d03b691c5d34c
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-15 21:51:28 +0900

    jsonファイルから推薦された色を読込む機能の実装

--- src/pages/ColorRecommendation/EvaluateIsColorPaintNext.tsx
+++ src/pages/ColorRecommendation/EvaluateIsColorPaintNext.tsx
@@ -1,9 +1,17 @@
 
 import inputOrderUsedColorScheme from "./data/inputOrderUsedColorsAmount.json";
+import outputRecommendColorsAmount from "./data/output/outputRecommendColorsAmount.json"
 
 // used[i][j]に対する推薦recommend[][]にused[i][j+1](次の色)が含まれているかどうか確認する関数
 export function isColorPaintNext(i: number, j: number): boolean {
-  console.log("inputOrderUsedColorScheme[" + i + "][" + j + "].color = " + inputOrderUsedColorScheme[i][j].color);
+  //console.log("inputOrderUsedColorScheme[" + i + "][" + (j+1) + "].color = " + inputOrderUsedColorScheme[i][j+1].color);
+  console.log("Color paint next is " + inputOrderUsedColorScheme[i][j + 1].color);
+
+  // used[i][j]のに対する推薦配色のセット(未実装)
+
+  //
+  console.log("outputRecommendColorsAmount = " + outputRecommendColorsAmount.filteredRecommendedColorSchemeAmount);
+
   return false;
 }
 
