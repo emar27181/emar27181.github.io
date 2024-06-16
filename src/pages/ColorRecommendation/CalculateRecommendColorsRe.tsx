134a51fae19f29f1c735e554e78200b26110244e
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-17 03:10:02 +0900

    配色群を保存する二重配列(recommendedColorSchemeAmount)の仮実装

--- src/pages/ColorRecommendation/CalculateRecommendColorsRe.tsx
+++ src/pages/ColorRecommendation/CalculateRecommendColorsRe.tsx
@@ -1,21 +1,35 @@
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper'
 //import { isUpdateRecommendColorsScheme } from './CalculateRecommendColors';
 import '../../App.css'
+import { ColorAmount } from '../../utils/ColorAmount';
 
 let isUpdateRecommendColorsScheme = true;
+//export let recommendedColorSchemeAmount: Array<Array<ColorAmount>> = [];
+export let recommendedColorSchemeAmount: ColorAmount[][] = [];
 
 export function CalculateRecommendColorsRe() {
   const sketch = (p: P5CanvasInstance) => {
 
+
     p.setup = () => {
+      initializeVariables();
       p.frameRate(1);
+    }
+
+    function initializeVariables() {
+      recommendedColorSchemeAmount[0] = [];
+      recommendedColorSchemeAmount[0].push(new ColorAmount(p.color(255, 255, 255, 0), 0));
     }
 
     p.draw = () => {
       if (isUpdateRecommendColorsScheme) {
-        console.log("called");
+        console.log(recommendedColorSchemeAmount);
       }
+
+      
     }
+
+
 
   }
 
