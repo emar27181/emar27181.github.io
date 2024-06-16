e283e2e6912f23f52ff364e3e827ae95f63eab58
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-17 02:29:59 +0900

    Jsonファイルを基に使用色を保存する配列を初期化するインポートと使用色を保存する配列の取得

--- src/pages/ColorRecommendation/CreateRecommendColorsAll.tsx
+++ src/pages/ColorRecommendation/CreateRecommendColorsAll.tsx
@@ -1,4 +1,5 @@
 
+import { initializeOrderUsedColorsAmountByJson } from "./CalculateUsedColors";
 import inputOrderUsedColorAmount from "./data/inputOrderUsedColorsAmount.json";
 
 // 使用色を保存するjsonファイルにある配色全てに対して，推薦する配色群を作成する関数
@@ -6,6 +7,10 @@ export function CreateRecommendColorsAll() {
 
   for (let i = 0; i < inputOrderUsedColorAmount.length; i++) {
     for (let j = 0; j < inputOrderUsedColorAmount[i].length; j++) {
+
+      let usedColorsAmount = initializeOrderUsedColorsAmountByJson(i, j);
+      console.log(usedColorsAmount);
+
       console.log("(" + i + "," + j + ")");
     }
   }
