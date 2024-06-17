6c09f8bcbcb652c7932f8e70ed315a0e63c35f83
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-18 02:16:34 +0900

    ボタンを押した際に推薦ログがエクスポートされる機能の実装

--- src/components/ButtonSaveColorSchemeAll.tsx
+++ src/components/ButtonSaveColorSchemeAll.tsx
@@ -1,16 +1,39 @@
 import React, { useState } from 'react';
 import { CreateRecommendColorsAll } from '../pages/ColorRecommendation/CreateRecommendColorsAll';
 
+const fileName = 'recommendColorSchemesAll';
+
 const ButtonSaveColorSchemeAll: React.FC = () => {
+  const [jsonURL, setJsonURL] = useState<string | null>(null);
 
   const handleExport = () => {
     console.log("ExportAll is clicked");
-    CreateRecommendColorsAll();
+    let jsonData = CreateRecommendColorsAll();
+
+    // 新しいBlobを作成
+    const blobData = new Blob([JSON.stringify(jsonData)], {
+      type: 'application/json',
+    });
+    const newJsonURL = URL.createObjectURL(blobData);
+
+    // Stateを更新してURLを更新
+    setJsonURL(newJsonURL);
+
+    // 自動でダウンロードリンクをクリックしてJSONファイルをダウンロード
+    const link = document.createElement('a');
+    link.href = newJsonURL;
+    link.download = `${fileName}.json`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
 
   return (
     <div>
       <button className="update-button" onClick={handleExport}> ExportAll</button>
+      {jsonURL && (
+        <a href={jsonURL} download={`${fileName}.json`} style={{ display: 'none' }} />
+      )}
     </div>
   );
 };
