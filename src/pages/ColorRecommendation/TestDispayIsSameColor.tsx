e605b496f75ef43d0b715253b1db1d758a583330
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-20 03:43:20 +0900

    同じ色の判定を行う関数の描画によるテストの初期実装

--- src/pages/ColorRecommendation/TestDispayIsSameColor.tsx
+++ src/pages/ColorRecommendation/TestDispayIsSameColor.tsx
@@ -1,21 +1,36 @@
 import '../../App.css'
 import { P5CanvasInstance, ReactP5Wrapper } from 'react-p5-wrapper';
 import React from 'react';
+import { DISPLAY_RATE } from '../../config/constants';
+import p5 from 'p5';
 
 export function TestDispayIsSameColor() {
   const sketch = (p: P5CanvasInstance) => {
 
-    const CANVAS_WIDTH = 256, CANVAS_HEIGHT = 256;
-    
+    const SQUARE_WIDTH = 0.1 * (DISPLAY_RATE * window.innerWidth / 3);
+
     p.setup = () => {
-      p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-      p.background(0); 
+      p.createCanvas(DISPLAY_RATE * window.innerWidth / 3, DISPLAY_RATE * window.innerWidth / 3);
+      p.background(0);
+      p.frameRate(1);
     };
 
     p.draw = () => {
-      p.fill(255); 
-      p.ellipse(p.width / 2, p.height / 2, 100, 100); 
+      compareColor(SQUARE_WIDTH, SQUARE_WIDTH, p.color("#FF0000"), p.color("#FF7700"));
     };
+
+    function compareColor(x: number, y: number, p5Color1: p5.Color, p5Color2: p5.Color) {
+      p.fill(p5Color1);
+      p.rect(x, y, SQUARE_WIDTH, SQUARE_WIDTH);
+
+
+      p.fill(p5Color2);
+      p.rect(x + SQUARE_WIDTH * 2, y, SQUARE_WIDTH, SQUARE_WIDTH);
+
+      p.fill(255);
+      p.noStroke();
+      p.text("text", x + SQUARE_WIDTH * 4, y + p.textSize());
+    }
   }
 
   return (
