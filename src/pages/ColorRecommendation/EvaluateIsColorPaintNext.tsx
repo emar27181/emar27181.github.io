963fa08f9620b17b0bf698af3ebf58e1c68bf4a8
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-19 23:34:26 +0900

    同じ色の判定をする関数の初期実装

--- src/pages/ColorRecommendation/EvaluateIsColorPaintNext.tsx
+++ src/pages/ColorRecommendation/EvaluateIsColorPaintNext.tsx
@@ -1,7 +1,9 @@
 
+import p5 from "p5";
 import inputOrderUsedColorScheme from "./data/inputOrderUsedColorsAmount.json";
 import outputRecommendColorsAmount from "./data/output/outputRecommendColorsAmount.json"
 import outputRecommendColorsAmountAll from "./data/output/outputRecommendColorsAmountAll.json"
+import { calculateLabColorSimilarity } from "./CalculateSimilarity";
 
 // used[i][j]に対する推薦recommend[][]にused[i][j+1](次の色)が含まれているかどうか確認する関数
 export function isColorPaintNext(colorSchemeNumber: number, colorNumber: number, recommendIndex: number): boolean {
@@ -22,6 +24,8 @@ export function isColorPaintNext(colorSchemeNumber: number, colorNumber: number,
   let dataRecomenndColorsAmount = outputRecommendColorsAmountAll[recommendIndex].dataRecommendColorsAmount;
   let recomenndColorsAmount = dataRecomenndColorsAmount.map(item => item.colorsAmount); // 配色を抽出し代入
 
+
+  const p = new p5(() => { });
   // recommend[][]にused[colorSchemeNumber][colorNumber+1](次の色)が含まれているかどうかの確認
   for (let i = 0; i < recomenndColorsAmount.length; i++) {
     // simValue: used[colorSchemeNumber][colorNumber]に対するrecommend[i]の相違度
@@ -35,9 +39,13 @@ export function isColorPaintNext(colorSchemeNumber: number, colorNumber: number,
     for (let j = 0; j < recomenndColorsAmount[i].length; j++) {
       compareCount++;
 
+      let p5Color1 = p.color(recomenndColorsAmount[i][j].color);
+      let p5Color2 = p.color(inputOrderUsedColorScheme[colorSchemeNumber][colorNumber + 1].color);
+
       // 推薦配色の中に次の色が含まれていた場合
-      if (recomenndColorsAmount[i][j].color === inputOrderUsedColorScheme[colorSchemeNumber][colorNumber + 1].color) {
-        console.log("推薦配色の中に次の色が含まれていました．(recomenndColorsAmount[" + i + "][" + j + "], compareCount = " + compareCount + ")");
+      if (isSameColor(p5Color1, p5Color2)) {
+        //if (recomenndColorsAmount[i][j].color === inputOrderUsedColorScheme[colorSchemeNumber][colorNumber + 1].color) {
+        console.log("推薦配色の中に次の色が含まれていました．(recomenndColorsAmount[" + i + "][" + j + "].color = " + recomenndColorsAmount[i][j].color + ", compareCount = " + compareCount + ")");
         return true;
       }
     }
@@ -46,6 +54,14 @@ export function isColorPaintNext(colorSchemeNumber: number, colorNumber: number,
   // 推薦配色の中に次の色が含まれていなかった場合
   console.log("推薦配色の中に次の色が含まれていませんでした．(compareCount = " + compareCount + ")");
   return false;
+}
+
+export function isSameColor(p5Color1: p5.Color, p5Color2: p5.Color): boolean {
+  const SIM_VALUE_SAME_COLOR = 10;
+
+  let simValue = calculateLabColorSimilarity(p5Color1, p5Color2);
+
+  return (simValue <= SIM_VALUE_SAME_COLOR);
 }
 
 // 生成された推薦する配色の評価をまとめて行う関数
