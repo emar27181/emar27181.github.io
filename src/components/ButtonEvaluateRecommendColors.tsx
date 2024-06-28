c070fd526e7fab7c6098cd979c1e5b94950306d5
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-29 03:04:29 +0900

    評価された結果(recall)を保存する際にファイル名に計算過程で使われた閾値が明記されるように変更

--- src/components/ButtonEvaluateRecommendColors.tsx
+++ src/components/ButtonEvaluateRecommendColors.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import '../App.css';
 import { evaluateRecommendColorSchemes } from '../pages/ColorRecommendation/EvaluateIsColorPaintNext';
+import { IS_EVALUATE_TIMING_DRAW_COLOR, SIM_VALUE_SAME_COLOR } from '../config/constants';
 
 const ButtonEvaluateRecommendColors: React.FC = () => {
 
@@ -18,10 +19,18 @@ const ButtonEvaluateRecommendColors: React.FC = () => {
     // Stateを更新してURLを更新
     setJsonURL(newJsonURL);
 
+    let timingStrArray = "";
+    for (let i = 0; i < IS_EVALUATE_TIMING_DRAW_COLOR.length; i++) {
+      timingStrArray += IS_EVALUATE_TIMING_DRAW_COLOR[i];
+
+      if (i != (IS_EVALUATE_TIMING_DRAW_COLOR.length - 1)) { timingStrArray += ", "; }
+    }
+    const FILE_NAME = "recall@k_SAME=" + SIM_VALUE_SAME_COLOR + "_TIME=[" + timingStrArray + "]_LIGHT=[-20, 20]";
+
     // 自動でダウンロードリンクをクリックしてJSONファイルをダウンロード
     const link = document.createElement('a');
     link.href = newJsonURL;
-    link.download = `recalls@k.json`;
+    link.download = FILE_NAME;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
