09a0eccc8195e19f49cd8802f9ac68b96c9f3a25
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-06-30 18:38:06 +0900

    同一色判定に用いる数値をHSLそれぞれの数値の差で視覚的にテストする機能の実装

--- src/pages/ColorRecommendation/TestDispayIsSameColor.tsx
+++ src/pages/ColorRecommendation/TestDispayIsSameColor.tsx
@@ -11,7 +11,7 @@ export function TestDispayIsSameColor() {
     const SQUARE_WIDTH = 0.07 * (DISPLAY_RATE * window.innerWidth / 3);
 
     p.setup = () => {
-      p.createCanvas(DISPLAY_RATE * window.innerWidth / 3, DISPLAY_RATE * window.innerWidth / 3);
+      p.createCanvas(DISPLAY_RATE * window.innerWidth / 3, 1.5 * DISPLAY_RATE * window.innerWidth / 3);
       p.background(0);
       p.frameRate(1);
       //p.noStroke();
@@ -23,25 +23,35 @@ export function TestDispayIsSameColor() {
       let count = 1;
 
       p.fill(255);
-      p.textSize(0.6*SQUARE_WIDTH);
-      p.text("SIM_VALUE_SAME_COLOR", 0.4 * p.width, 0.7 * SQUARE_WIDTH);
+      p.textSize(0.6 * SQUARE_WIDTH);
+      p.text("diff", 0.35 * p.width, 0.7 * SQUARE_WIDTH);
 
-      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), p.color("#FF0000"), p.color("#FF7700"));
-      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), p.color("#FF0000"), p.color("#FF5500"));
-      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), p.color("#FF0000"), p.color("#FF4400"));
-      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), p.color("#FF0000"), p.color("#FF2200"));
+      // 色相の差
+      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(14, 100, 50));
+      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(20, 100, 50));
+      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(25, 100, 50));
+      count++;
 
-      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 60, 50));
-      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 75, 50));
+      // 彩度の差(->薄)
+      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 83, 50));
+      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 65, 50));
+      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 50, 50));
+      count++;
 
+      // 明度の差(->明)
+      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 100, 59));
+      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 100, 65));
       compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 100, 70));
-      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 100, 60));
+      count++;
 
-      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 100, 30));
-      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 100, 40));
+      // 明度の差(->暗)
+      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 100, 45));
+      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 100, 41));
+      compareColor(0.5 * SQUARE_WIDTH, SQUARE_WIDTH * (count++), hslToP5Color(0, 100, 50), hslToP5Color(0, 100, 36));
+      count++;
     };
 
-    function hslToP5Color(h: number, s: number, l: number): p5.Color{
+    function hslToP5Color(h: number, s: number, l: number): p5.Color {
       p.colorMode(p.HSL);
       return (p.color(h, s, l));
     }
@@ -56,7 +66,7 @@ export function TestDispayIsSameColor() {
       p.rect(x + SQUARE_WIDTH * 2, y, SQUARE_WIDTH, SQUARE_WIDTH);
 
       p.fill(255);
-      p.textSize(0.8*SQUARE_WIDTH);
+      p.textSize(0.8 * SQUARE_WIDTH);
       p.text(":", x + 1.3 * SQUARE_WIDTH, y + p.textSize());
       p.text("→ " + p.round(simValue), x + SQUARE_WIDTH * 3.5, y + p.textSize());
     }
