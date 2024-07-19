77fbb02a2e17abf5d27c5421fe8667f6930f31af
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-07-19 22:56:58 +0900

    バリエーションの明度を任意に設定し推薦配色を生成する機能の実装

--- src/components/ButtonSaveColorSchemeAll.tsx
+++ src/components/ButtonSaveColorSchemeAll.tsx
@@ -5,9 +5,10 @@ const fileName = 'recommendColorSchemesAll';
 
 
 // 引数で受け取る基準で生成した推薦配色群を保存する関数
-function downloadColorSchemesJson( setJsonURL: React.Dispatch<React.SetStateAction<string | null>>) {
+function downloadColorSchemesJson(variation_lightness_diffs: number[], setJsonURL: React.Dispatch<React.SetStateAction<string | null>>) {
 
-  let jsonData = CreateRecommendColorsAll(1, 3, [-10, 10, -20, 20]);
+
+  let jsonData = CreateRecommendColorsAll(1, 3, variation_lightness_diffs);
 
   // 新しいBlobを作成
   const blobData = new Blob([JSON.stringify(jsonData)], {
@@ -18,10 +19,12 @@ function downloadColorSchemesJson( setJsonURL: React.Dispatch<React.SetStateActi
   // Stateを更新してURLを更新
   setJsonURL(newJsonURL);
 
+  const FILE_NAME = "outputRecommendColorsSchemeAll" + "_LIGHT=[" + variation_lightness_diffs +  "]";
+
   // 自動でダウンロードリンクをクリックしてJSONファイルをダウンロード
   const link = document.createElement('a');
   link.href = newJsonURL;
-  link.download = `${fileName}.json`;
+  link.download = FILE_NAME;
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
@@ -32,7 +35,11 @@ const ButtonSaveColorSchemeAll: React.FC = () => {
 
   const handleExport = () => {
     console.log("ExportAll is clicked");
-    downloadColorSchemesJson(setJsonURL);
+
+    downloadColorSchemesJson([], setJsonURL);
+    downloadColorSchemesJson([-10, 10], setJsonURL);
+    downloadColorSchemesJson([-20, 20], setJsonURL);
+    downloadColorSchemesJson([-10, 10,-20, 20], setJsonURL);
   };
 
   return (
