62886c4d514f1c0c2770cff3d628840eab91067b
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-07-19 17:32:32 +0900

    引数で受け取る評価基準で評価したデータを保存する関数の初期実装

--- src/components/ButtonEvaluateRecommendColors.tsx
+++ src/components/ButtonEvaluateRecommendColors.tsx
@@ -3,43 +3,49 @@ import '../App.css';
 import { evaluateRecommendColorSchemes } from '../pages/ColorRecommendation/EvaluateIsColorPaintNext';
 import { IS_EVALUATE_TIMING_DRAW_COLOR, SIM_VALUE_SAME_COLOR, VARIATIONS_LIGHTNESS_DIFF } from '../config/constants';
 
+
+// 引数で受け取る評価基準で評価したデータを保存する関数
+function downloadEvaluateJson(setJsonURL: React.Dispatch<React.SetStateAction<string | null>>){
+  let jsonData = evaluateRecommendColorSchemes();
+
+  // 新しいBlobを作成
+  const blobData = new Blob([JSON.stringify(jsonData)], {
+    type: 'application/json',
+  });
+  const newJsonURL = URL.createObjectURL(blobData);
+
+  // Stateを更新してURLを更新
+  setJsonURL(newJsonURL);
+
+  let timingStrArray = "";
+  let lighnessDiffStrArray = "";
+  for (let i = 0; i < IS_EVALUATE_TIMING_DRAW_COLOR.length; i++) {
+    timingStrArray += IS_EVALUATE_TIMING_DRAW_COLOR[i];
+
+    if (i != (IS_EVALUATE_TIMING_DRAW_COLOR.length - 1)) { timingStrArray += ", "; }
+  }
+  for (let i = 0; i < VARIATIONS_LIGHTNESS_DIFF.length; i++) {
+    lighnessDiffStrArray += VARIATIONS_LIGHTNESS_DIFF[i];
+    if (i != (VARIATIONS_LIGHTNESS_DIFF.length - 1)) { lighnessDiffStrArray += ", "; }
+  }
+
+  const FILE_NAME = "recall@k_SAME=" + SIM_VALUE_SAME_COLOR + "_TIME=[" + timingStrArray + "]_LIGHT=[" + lighnessDiffStrArray + "]";
+
+  // 自動でダウンロードリンクをクリックしてJSONファイルをダウンロード
+  const link = document.createElement('a');
+  link.href = newJsonURL;
+  link.download = FILE_NAME;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
 const ButtonEvaluateRecommendColors: React.FC = () => {
 
   const [jsonURL, setJsonURL] = useState<string | null>(null);
 
   const handleClick = () => {
-    let jsonData = evaluateRecommendColorSchemes();
-
-    // 新しいBlobを作成
-    const blobData = new Blob([JSON.stringify(jsonData)], {
-      type: 'application/json',
-    });
-    const newJsonURL = URL.createObjectURL(blobData);
-
-    // Stateを更新してURLを更新
-    setJsonURL(newJsonURL);
-
-    let timingStrArray = "";
-    let lighnessDiffStrArray = "";
-    for (let i = 0; i < IS_EVALUATE_TIMING_DRAW_COLOR.length; i++) {
-      timingStrArray += IS_EVALUATE_TIMING_DRAW_COLOR[i];
-
-      if (i != (IS_EVALUATE_TIMING_DRAW_COLOR.length - 1)) { timingStrArray += ", "; }
-    }
-    for (let i = 0; i < VARIATIONS_LIGHTNESS_DIFF.length; i++) {
-      lighnessDiffStrArray += VARIATIONS_LIGHTNESS_DIFF[i];
-      if (i != (VARIATIONS_LIGHTNESS_DIFF.length - 1)) { lighnessDiffStrArray += ", "; }
-    }
-
-    const FILE_NAME = "recall@k_SAME=" + SIM_VALUE_SAME_COLOR + "_TIME=[" + timingStrArray + "]_LIGHT=[" + lighnessDiffStrArray + "]";
-
-    // 自動でダウンロードリンクをクリックしてJSONファイルをダウンロード
-    const link = document.createElement('a');
-    link.href = newJsonURL;
-    link.download = FILE_NAME;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    downloadEvaluateJson(setJsonURL);
   };
 
   return (
