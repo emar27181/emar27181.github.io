1cfbd8b4c2c6f29924ae1e948a29d621715607c7
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-07-19 22:13:06 +0900

    引数で受け取る基準で生成した推薦配色群を保存する関数の初期実装

--- src/components/ButtonSaveColorSchemeAll.tsx
+++ src/components/ButtonSaveColorSchemeAll.tsx
@@ -3,29 +3,36 @@ import { CreateRecommendColorsAll } from '../pages/ColorRecommendation/CreateRec
 
 const fileName = 'recommendColorSchemesAll';
 
+
+// 引数で受け取る基準で生成した推薦配色群を保存する関数
+function downloadColorSchemesJson( setJsonURL: React.Dispatch<React.SetStateAction<string | null>>) {
+
+  let jsonData = CreateRecommendColorsAll();
+
+  // 新しいBlobを作成
+  const blobData = new Blob([JSON.stringify(jsonData)], {
+    type: 'application/json',
+  });
+  const newJsonURL = URL.createObjectURL(blobData);
+
+  // Stateを更新してURLを更新
+  setJsonURL(newJsonURL);
+
+  // 自動でダウンロードリンクをクリックしてJSONファイルをダウンロード
+  const link = document.createElement('a');
+  link.href = newJsonURL;
+  link.download = `${fileName}.json`;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
 const ButtonSaveColorSchemeAll: React.FC = () => {
   const [jsonURL, setJsonURL] = useState<string | null>(null);
 
   const handleExport = () => {
     console.log("ExportAll is clicked");
-    let jsonData = CreateRecommendColorsAll();
-
-    // 新しいBlobを作成
-    const blobData = new Blob([JSON.stringify(jsonData)], {
-      type: 'application/json',
-    });
-    const newJsonURL = URL.createObjectURL(blobData);
-
-    // Stateを更新してURLを更新
-    setJsonURL(newJsonURL);
-
-    // 自動でダウンロードリンクをクリックしてJSONファイルをダウンロード
-    const link = document.createElement('a');
-    link.href = newJsonURL;
-    link.download = `${fileName}.json`;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    downloadColorSchemesJson(setJsonURL);
   };
 
   return (
