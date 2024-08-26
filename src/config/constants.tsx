78c5b740b2c23919242644f15cf75e8370c44da8
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-08-26 17:32:55 +0900

    コンポーネントのサイズ倍率を調整

--- src/config/constants.tsx
+++ src/config/constants.tsx
@@ -1,4 +1,4 @@
-export const DISPLAY_RATE = 0.42;
+export const DISPLAY_RATE = 0.48;
 export const DISPLAY_CANVAS_FILTER_RATE = 0.7;
 export const DISPLAY_USED_COLOR_RATIO_RATE = 0;
 export const DISPLAY_USED_COLOR_WHEEL_RATE = 2;
