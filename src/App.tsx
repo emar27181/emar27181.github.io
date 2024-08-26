c141690a6ea2c39946c11e6c324923890206b27e
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-08-26 16:21:53 +0900

    カラーパレットが表示されないキャンバスページの初期実装

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,7 @@ import GravityHome from './views/GravityHome';
 import GyroCanvasHome from './views/GyroCanvasHome';
 import EvaluateResearchPage from './views/EvaluateResearchPage';
 import CanvasHomeForEntertainmentComputing2024 from './views/CanvasHomeForEntertainmentComputing2024';
+import CanvasHomeForEntertainmentComputing2024_none from './views/CanvasHomeForEntertainmentComputing2024_none';
 
 let isDesktop = false;
 let isMobileLandscape = false;
@@ -120,6 +121,7 @@ function App() {
             {/*<Nav.Link href='/gravityHome' > 重力操作 </Nav.Link>*/}
 
             <Nav.Link href='/canvasHomeForEntertainmentComputing2024' > EC2024 </Nav.Link>
+            <Nav.Link href='/canvasHomeForEntertainmentComputing2024_none' > EC2024_none </Nav.Link>
             {/*<Nav.Link href='/canvasHome' > 研究 </Nav.Link>*/}
             <Nav.Link href="/workMenu" >Works</Nav.Link>
             {/*<Nav.Link href='/canvasHomeForInteraction2024' > キャンバス </Nav.Link>*/}
@@ -201,8 +203,14 @@ function App() {
           <CanvasHomeForInteraction2024 />
         </Route>
 
+
+
         <Route path="/canvasHomeForEntertainmentComputing2024">
           <CanvasHomeForEntertainmentComputing2024 />
+        </Route>
+
+        <Route path="/canvasHomeForEntertainmentComputing2024_none">
+          <CanvasHomeForEntertainmentComputing2024_none />
         </Route>
 
         <Route path="/canvasHomeRecommend">
